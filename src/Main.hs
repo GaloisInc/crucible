fbd5a106ecd562e52835c1203ac70a3553d281d9
Brian Ledger <brian@dyn-40-139.galois.com>
2012-10-11 16:22:13 -0700

    Skeletal lexing, parsing, evaluation, typing... also Makefiles

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,54 @@
 module Main where
 
-import SAWScript.Parser
+import System.Console.Haskeline
+import System.Exit
+
+import SAWScript.MyParser  ( stringToExpr )
+import SAWScript.Evaluator ( evaluate )
+import SAWScript.AST       ( reflect )
+
+echo s = do outputStrLn s; return ()
 
 main :: IO ()
-main = putStrLn "not done yet"
+main = do
+  putStrLn "SAWScript, Version 2.0.0, :? for help\n"
+  putStrLn "\n .oooooo..o       .o.    oooooo   oooooo     oooo "
+  putStrLn "d8P'    `Y8      .888.    `888.    `888.     .8'  "
+  putStrLn "Y88bo.          .8\"888.    `888.   .8888.   .8'   "
+  putStrLn " `\"Y8888o.     .8' `888.    `888  .8'`888. .8'    "
+  putStrLn "     `\"Y88b   .88ooo8888.    `888.8'  `888.8'     "
+  putStrLn "oo     .d8P  .8'     `888.    `888'    `888'      "
+  putStrLn "8\"\"88888P'  o88o     o8888o    `8'      `8'       \n"
+  runInputT defaultSettings loop
+    where 
+      loop :: InputT IO ()
+      loop = do
+        minput <- getInputLine "> "
+        case minput of
+          Nothing      -> loop
+          Just (":q")  -> do
+                            echo "Leaving SAWScript"
+                            return ()
+          Just (':':s) -> do
+                            processDirective s
+                            loop
+          Just line    -> case stringToExpr line of
+                            Left error -> do echo error; loop
+                            Right ast  -> do
+                                            let val = evaluate ast
+                                            echo (reflect val)
+                                            loop
+
+processDirective :: String -> InputT IO ()
+processDirective s = case s of
+  "h"       -> printHelp
+  "?"       -> printHelp
+  otherwise -> do echo ("Unrecognized directive: ':"++s++"'."); return ()
+
+printHelp :: InputT IO ()
+printHelp = do --TODO: Fill in directives as they are developed.
+  echo " Commands available from the prompt:\n"
+  echo " -- Commands for debugging:\n"
+  echo " -- Commands for changing settings:\n"
+  echo " -- Commands for displaying information:\n"
+  return ()
--- src/SAWScript/AST.hs
+++ src/SAWScript/AST.hs
@@ -1,14 +1,40 @@
-module AST where
+module SAWScript.AST where
 
-data BitField = [Bool]
+import Data.List
+
+type BitField = [Bool]
 
 data Expr =
     BitLiteral BitField
   | Binder String Expr
   | Bag [Expr]
 
+instance Show Expr where
+  show (BitLiteral bools) = "'" ++ (map (\b -> if b then '1' else '0') bools)
+  show (Binder name val)  = name ++ " = " ++ (show val)
+  show (Bag exprs)        = "{ " ++ (intercalate "; " (map show exprs)) ++ " }"
+
+reflect :: Expr -> String
+reflect e@(BitLiteral _)  = "_ = " ++ show e ++ " :: " ++ check e
+reflect (Binder name val) = name ++ " = " ++ (show val) ++ " :: " ++ (check val)
+reflect (Bag exprs)       = "_ :: { " ++ (intercalate "; " (nub (map checkFields exprs))) ++ " }"
+
+checkFields e@(BitLiteral _)    = check e
+checkFields e@(Binder name val) = name ++ " :: " ++ (check val)
+checkFields e@(Bag _)           = check e
+
+check :: Expr -> String
+check (BitLiteral bits) = "["++ (show .length $ bits) ++"]"
+check (Binder name val) = check val
+check (Bag exprs)       = "{ " ++ ((intercalate "; ") . nub . (map checkFields) $ exprs) ++ " }"
 
 
+
+
+
+
+
+-- IGNORE
 
 toCubitType :: Expr -> String
 toCubitType (BitLiteral bits)  
