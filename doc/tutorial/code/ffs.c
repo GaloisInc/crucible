3649d3114d9dc6411c78c76873a68e4b789185e1
Aaron Tomb <atomb@galois.com>
2013-06-06 09:09:49 -0700

    Add preliminary SAWScript tutorial with examples.
    Currently, the tutorial is just a copy of the one written for the
    original SAWScript, and the examples in the code directory aren't
    referenced from the tutorial. That will change.

---
+++ doc/tutorial/code/ffs.c
@@ -0,0 +1,18 @@
+int ffs_ref(int word) {
+    int i = 0;
+    if(!word)
+        return 0;
+    for(int cnt = 0; cnt < 32; cnt++)
+        if(((1 << i++) & word) != 0)
+            return i;
+    return 0;
+}
+
+int ffs_imp(int i) {
+    char n = 1;
+    if (!(i & 0xffff)) { n += 16; i >>= 16; }
+    if (!(i & 0x00ff)) { n += 8;  i >>= 8; }
+    if (!(i & 0x000f)) { n += 4;  i >>= 4; }
+    if (!(i & 0x0003)) { n += 2;  i >>= 2; }
+    return (i) ? (n+((i+1) & 0x01)) : 0;
+}
