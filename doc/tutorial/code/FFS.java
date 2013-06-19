787af88ffc2d23c4dd726b4ac324cb388414fd45
Aaron Tomb <atomb@galois.com>
2013-06-18 18:05:21 -0700

    Remove obsolete hack in FFS Java example.

--- doc/tutorial/code/FFS.java
+++ doc/tutorial/code/FFS.java
@@ -11,10 +11,10 @@ class FFS {
 
     static int ffs_imp(int i) {
         byte n = 1;
-        if ((i & 0xffff) == 0) { n += 16; i >>= 16; } else { n = n; }
-        if ((i & 0x00ff) == 0) { n +=  8; i >>=  8; } else { n = n; }
-        if ((i & 0x000f) == 0) { n +=  4; i >>=  4; } else { n = n; }
-        if ((i & 0x0003) == 0) { n +=  2; i >>=  2; } else { n = n; }
+        if ((i & 0xffff) == 0) { n += 16; i >>= 16; }
+        if ((i & 0x00ff) == 0) { n +=  8; i >>=  8; }
+        if ((i & 0x000f) == 0) { n +=  4; i >>=  4; }
+        if ((i & 0x0003) == 0) { n +=  2; i >>=  2; }
         if (i != 0) { return (n+((i+1) & 0x01)); } else { return 0; }
     }
 }
