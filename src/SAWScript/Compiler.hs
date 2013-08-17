f98749033c1359169eb8331139231339c7516458
Benjamin Barenblat <bbarenblat@galois.com>
2013-08-17 11:50:34 -0700

    Rework 'Err' as a monad transformer

--- src/SAWScript/Compiler.hs
+++ src/SAWScript/Compiler.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module SAWScript.Compiler ( Compiler, compiler, runCompiler
-                          , Err, runErr
+                          , Err, runErr, runErr'
+                          , ErrT, runErrT, runErrT', mapErrT
                           ) where
 
 import Control.Applicative (Alternative, Applicative)
 import Control.Monad (MonadPlus)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Trans.Error (ErrorT, runErrorT)
+import Control.Monad.Trans.Class (MonadTrans)
+import Control.Monad.Trans.Error (ErrorT, mapErrorT, runErrorT)
 
 import Text.Show.Pretty (ppShow)
 
@@ -24,19 +26,42 @@ reportError = putStrLn . ("Error\n" ++) . indent 2
 
 type Compiler a b = a -> Err b
 
-newtype Err b = Err { extractErrorT :: ErrorT String IO b }
-              deriving (Functor, Applicative, Alternative, Monad, MonadPlus,
-                        MonadIO)
+{-| This is a slightly unusual formulation--'Err' is defined in terms of
+'ErrT', but the inner monad is 'IO', not 'Identity'--for backward compatibility
+reasons. -}
+-- TODO: Make 'Err' use 'Identity' instead of 'IO'.
+type Err = ErrT IO
 
+newtype ErrT m b = ErrT { extractErrorT :: ErrorT String m b }
+                 deriving (Functor, Applicative, Alternative, Monad, MonadPlus,
+                           MonadIO, MonadTrans)
+
+{-| 'runErr' uses continuation-passing style for backward compatibility. -}
+-- TODO: Get rid of this.
 runErr :: Err a -> (String -> IO r) -> (a -> IO r) -> IO r
-runErr m fl sc = do
-  result <- runErrorT $ extractErrorT m
+runErr = runErrT
+
+runErr' :: Err a -> IO (Either String a)
+runErr' = runErrT'
+
+{-| 'runErrT' uses continuation-passing style to present a interface uniform
+with 'runErr'. -}
+-- TODO: Once 'runErr' has been eliminated, replace this with 'runErrT''.
+runErrT :: (Monad m) => ErrT m a -> (String -> m r) -> (a -> m r) -> m r
+runErrT m fl sc = do
+  result <- runErrT' m
   either fl sc result
+
+runErrT' :: (Monad m) => ErrT m a -> m (Either String a)
+runErrT' = runErrorT . extractErrorT
+
+mapErrT :: (m (Either String a) -> n (Either String b)) -> ErrT m a -> ErrT n b
+mapErrT f = ErrT . mapErrorT f . extractErrorT
 
 compiler :: Show a => String -> Compiler a b -> Compiler a b
 compiler name comp input = do
   result <- liftIO $ runErrorT $ extractErrorT $ comp input
-  Err $ case result of
+  ErrT $ case result of
     Left err -> fail $
       unlines [ name ++ ": " ++ err
               , "in:"
