fce811b168fef5e8edbf2fdaa710e40c6784387f
Benjamin Barenblat <bbarenblat@galois.com>
2013-08-19 13:00:51 -0700

    REPL can evaluate functions called 'main'

--- src/SAWScript/REPL.hs
+++ src/SAWScript/REPL.hs
@@ -1,14 +1,40 @@
 module SAWScript.REPL where
 
+import Control.Monad ((>=>))
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.IO.Class (liftIO)
+import Data.Foldable (foldrM)
+import qualified Data.Map as Map
 import System.Console.Haskeline (InputT, runInputT)
 import qualified System.Console.Haskeline as Haskeline
 
-import SAWScript.AST (TopStmtSimple, RawT)
+import SAWScript.AST (moduleNameFromString)
+import SAWScript.BuildModules (buildModules)
 import SAWScript.Compiler (runCompiler, ErrT, mapErrT)
 import SAWScript.Import (loadModuleFromString, preludeLoadedModules)
+import SAWScript.Interpreter (interpretMain)
 import SAWScript.Options (Options)
+
+-- TODO: Get rid of all this!
+import Control.Applicative
+import Control.Monad
+import qualified Data.Map      as M
+import qualified Data.Foldable as F
+import qualified Data.Set      as S
+import Data.List
+import System.IO
+import System.Console.GetOpt
+import System.Environment
+import System.FilePath
+import SAWScript.AST
+import SAWScript.BuildModules as BM
+import SAWScript.Compiler
+import SAWScript.Import
+import SAWScript.Interpreter
+import SAWScript.MGU (checkModule)
+import SAWScript.Options
+import SAWScript.RenameRefs as RR
+import SAWScript.ResolveSyns
 
 run :: Options -> IO ()
 run options = runInputT Haskeline.defaultSettings loop
@@ -26,10 +52,29 @@ evaluateAndPrint :: Options -> String -> ErrT (InputT IO) ()
 evaluateAndPrint options s = do
   preexistingModules <- liftIO preludeLoadedModules
   loadModuleFromString options "<stdin>" s preexistingModules $
-    \mods -> do
-      {- TODO: Actually do something useful here instead of just printing the
-      modules -}
-      lift $ Haskeline.outputStrLn $ showResult mods
+    mapErrT liftIO . buildModules >=>
+    mapErrT liftIO . foldrM checkModuleWithDeps Map.empty >=>
+    liftIO . interpretMain options . Map.findWithDefault (error "wtf") (moduleNameFromString "<stdin>") >=>
+    lift . Haskeline.outputStrLn . showResult
 
 showResult :: (Show a) => a -> String
 showResult = show
+
+-- TODO: This is duplicate code from 'Main'.  Factor it out.
+checkModuleWithDeps :: BM.ModuleParts (ExprSimple RawT)
+  -> M.Map ModuleName ValidModule
+  -> Err (M.Map ModuleName ValidModule)
+checkModuleWithDeps (BM.ModuleParts mn ee pe te ds) cms =
+  mod >>=
+  resolveSyns >>=
+  renameRefs  >>=
+  checkModule >>= \cm -> return $ M.insert mn cm cms
+  where
+  deps :: Err (M.Map ModuleName ValidModule)
+  deps = fmap M.fromList $ forM (S.toList ds) $ \n ->
+           case M.lookup n cms of
+             Nothing -> fail $ "Tried to compile module " ++ show mn ++
+                               " before compiling its dependency, " ++ show n
+             Just m  -> return (n,m)
+  mod  = Module mn ee pe te <$> deps
+
