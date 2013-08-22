daf892c1d4e12a25acd2d188fecedc598f643736
Benjamin Barenblat <bbarenblat@galois.com>
2013-08-21 17:49:15 -0700

    REPL resolves type synonyms on input

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,13 @@
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import qualified Data.Map      as M
-import qualified Data.Foldable as F
-import qualified Data.Set      as S
 import Data.List
 
 import System.IO
 import System.Console.GetOpt
 import System.Environment
-import System.FilePath
 
-import SAWScript.AST
-import SAWScript.BuildModules as BM
-import SAWScript.Compiler
-import SAWScript.Import
-import SAWScript.Interpreter
-import SAWScript.MGU (checkModule)
 import SAWScript.Options
-import SAWScript.RenameRefs as RR
-import SAWScript.ResolveSyns
+import SAWScript.ProcessFile (processFile)
 import qualified SAWScript.REPL as REPL
 
 main :: IO ()
@@ -30,6 +17,8 @@ main = do
     (opts, files, []) -> do
       let opts' = foldl' (flip id) defaultOptions opts
       opts'' <- processEnv opts'
+      {- We have two modes of operation: batch processing, handled in
+      'SAWScript.ProcessFile', and a REPL, defined in 'SAWScript.REPL'. -}
       case (runInteractively opts'', files) of
         (True, []) -> REPL.run -- TODO: pass opts''
         (False, file:_) -> processFile opts'' file
@@ -40,53 +29,3 @@ main = do
     (_, _, errs)      ->
       hPutStrLn stderr (concat errs ++ usageInfo header options)
   where header = "Usage: saw [OPTION...] [-I | files...]"
-
-
-
-processFile :: Options -> FilePath -> IO ()
-
-processFile opts file | takeExtensions file == ".saw" = do
-  when (verbLevel opts > 0) $ putStrLn $ "Processing SAWScript file " ++ file
-  {-
-  loadPrelude opts $ \lms -> do
-    processModule opts lms (ModuleName [] "Prelude")
-  -}
-  loadWithPrelude opts file $ \loadedModules -> do
-    let modName = moduleNameFromPath file
-    processModule opts loadedModules modName
-
-processFile _ file = putStrLn $ "Don't know how to handle file " ++ file
-
-
-
-processModule :: Options -> LoadedModules -> ModuleName -> IO ()
-processModule opts lms modName =
-  -- TODO: merge the two representations of the prelude into one
-  --  that both the renamer and the type checker can understand.
-  runCompiler comp lms (interpretMain opts)
-  where
-  comp =     buildModules
-         >=> F.foldrM checkModuleWithDeps M.empty
-         >=> (\cms -> case M.lookup modName cms of
-               Just cm -> return cm
-               Nothing -> fail $ "Module " ++ show modName ++
-                                 " not found in environment of checkedModules:" ++
-                                 show (M.keys cms))
-
-
-checkModuleWithDeps :: BM.ModuleParts (ExprSimple RawT)
-  -> M.Map ModuleName ValidModule
-  -> Err (M.Map ModuleName ValidModule)
-checkModuleWithDeps (BM.ModuleParts mn ee pe te ds) cms =
-  mod >>=
-  resolveSyns >>=
-  renameRefs  >>=
-  checkModule >>= \cm -> return $ M.insert mn cm cms
-  where
-  deps :: Err (M.Map ModuleName ValidModule)
-  deps = fmap M.fromList $ forM (S.toList ds) $ \n ->
-           case M.lookup n cms of
-             Nothing -> fail $ "Tried to compile module " ++ show mn ++
-                               " before compiling its dependency, " ++ show n
-             Just m  -> return (n,m)
-  mod  = Module mn ee pe te <$> deps
--- src/SAWScript/REPL.hs
+++ src/SAWScript/REPL.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module SAWScript.REPL where
 
-import Control.Monad ((=<<))
 import Control.Monad.IO.Class (liftIO)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Foldable (foldrM)
 import System.Console.Haskeline (InputT, runInputT)
 import qualified System.Console.Haskeline as Haskeline
 
-import SAWScript.AST (BlockStmt, UnresolvedName, RawT)
+import SAWScript.AST (ModuleName,
+                      Module(moduleTypeEnv), ValidModule,
+                      BlockStmt,
+                      UnresolvedName, ResolvedName,
+                      RawT, ResolvedT)
+import SAWScript.BuildModules (buildModules, preludeName)
 import SAWScript.Compiler (runCompiler, ErrT, mapErrT)
+import SAWScript.Import (preludeLoadedModules)
 import SAWScript.Lexer (scan)
 import SAWScript.Parser (parseBlockStmt)
+import SAWScript.ProcessFile (checkModuleWithDeps)
+import SAWScript.ResolveSyns (resolveBlockStmtSyns)
 
 run :: IO ()
 run = runInputT Haskeline.defaultSettings loop
@@ -22,11 +33,24 @@ run = runInputT Haskeline.defaultSettings loop
                 Haskeline.outputStrLn $ showResult r
               loop
 
-evaluate :: String -> ErrT (InputT IO) (BlockStmt UnresolvedName RawT)
+evaluate :: String -> ErrT (InputT IO) (BlockStmt UnresolvedName ResolvedT)
 evaluate line = do
+  -- Load all the modules that should be in scope when evaluating this line.
+  modsInScope :: Map ModuleName ValidModule
+              <-
+    liftIO preludeLoadedModules >>=
+    mapErrT liftIO . buildModules >>=
+    mapErrT liftIO . foldrM checkModuleWithDeps Map.empty
+  -- Lex and parse.
   tokens <- mapErrT liftIO $ scan "<stdin>" line
-  statement <- mapErrT liftIO $ parseBlockStmt tokens
-  return statement
+  ast :: BlockStmt UnresolvedName RawT
+      <- mapErrT liftIO $ parseBlockStmt tokens
+  {- Resolve type synonyms, abstract types, etc.  They're not supported by the
+  REPL, so there never are any. -}
+  synsResolved :: BlockStmt UnresolvedName ResolvedT
+               <- mapErrT liftIO $ resolveBlockStmtSyns Map.empty ast
+  -- All done.
+  return synsResolved
 
 showResult :: Show a => a -> String
 showResult = show
