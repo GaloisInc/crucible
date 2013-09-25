def4513f4ace8e9722322644eda3b6132215c28e
Aaron Tomb <atomb@galois.com>
2013-09-25 12:19:35 -0700

    Add some LLVM method spec examples.

---
+++ examples/llvm/global.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+uint32_t g;
+
+void clear() {
+    g = 0;
+}
+
+void set(uint32_t x) {
+    g = x;
+}
---
+++ examples/llvm/ptr.c
@@ -0,0 +1,9 @@
+int add(int *x, int *y) {
+    return *x + *y;
+}
+
+void clear(int a[10]) {
+    for(int i = 0; i < 10; i++) {
+        a[i] = 0;
+    }
+}
