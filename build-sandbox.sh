798cf3fa1ea44a11998015c412049354acf25c34
Aaron Tomb <atomb@galois.com>
2014-06-19 14:59:04 -0700

    Fix re-running build-sandbox.sh with -t.

--- build-sandbox.sh
+++ build-sandbox.sh
@@ -10,11 +10,13 @@ cabal_flags="--reinstall --force-reinstalls"
 test_flags="--enable-tests --run-tests --disable-library-coverage"
 dotests="false"
 dopull="false"
+sandbox_dir=build
 while getopts "tp" opt; do
   case $opt in
     t)
       cabal_flags="${cabal_flags} ${test_flags}"
       dotests="true"
+      sandbox_dir=build-tests
       ;;
     p) dopull="true" ;;
     \?)
@@ -26,6 +28,14 @@ done
 
 if [ ! -e ./deps ] ; then
   mkdir deps
+fi
+
+cabal sandbox --sandbox=${sandbox_dir} init
+
+if [ "${dotests}" == "true" ] ; then
+  for pkg in sawScript cryptol-verifier llvm-verifier jvm-verifier saw-core Verinf ; do
+    cabal sandbox hc-pkg unregister $pkg || true
+  done
 fi
 
 for repo in ${REPOS} ; do
@@ -46,21 +56,17 @@ for repo in ${GITHUB_REPOS} ; do
   fi
 done
 
-if [ ! -e ./build ] ; then
-  cabal sandbox --sandbox=./build init
-fi
-
 for repo in ${REPOS} ${GITHUB_REPOS} ; do
   cabal sandbox add-source deps/${repo}
   if [ "${dotests}" == "true" ] ; then
-    cabal install ${repo} ${cabal_flags}
+    cabal install --force ${repo} ${cabal_flags}
   fi
 done
 
 for pkg in ${PKGS} ; do
   cabal sandbox add-source ../${pkg}
   if [ "${dotests}" == "true" ] ; then
-    cabal install ../${pkg} ${cabal_flags}
+    cabal install --force ../${pkg} ${cabal_flags}
   fi
 done
 
