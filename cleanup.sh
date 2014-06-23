39add840acfd66ed7b138aa2ad918b8e9cf633db
Aaron Tomb <atomb@galois.com>
2014-06-23 15:02:04 -0700

    Add script to clean up old builds.

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+(cd deps/Aiger && cabal clean)
+(cd deps/abcBridge/abc && rm -f `find . -name "*.o"`)
+(cd deps/abcBridge && cabal clean)
+(cd deps/aig && cabal clean)
+(cd deps/cryptol && cabal clean)
+(cd deps/jvm-parser && cabal clean)
+(cd deps/llvm-pretty && cabal clean)
+(cd ../Cryptol && cabal clean)
+(cd ../Java && cabal clean)
+(cd ../LLVM && cabal clean)
+(cd ../SAWCore && cabal clean)
+(cd ../Verinf && cabal clean)
+cabal clean
+rm -rf build
+rm -rf build-test
