2ba66f7265819e5ccc7b852ab1f4510396083d21
Aaron Tomb <atomb@galois.com>
2014-07-28 15:14:06 -0700

    Complain if LLVM argument mentioned multiple times

--- src/SAWScript/LLVMMethodSpec.hs
+++ src/SAWScript/LLVMMethodSpec.hs
@@ -682,7 +682,10 @@ initializeVerification sc ir = do
 
   gm <- use globalTerms
   let rreg =  (,Ident "__sawscript_result") <$> sdRetType fnDef
-      cmpFst (i, _) (i', _) = i `compare` i'
+      cmpFst (i, _) (i', _) = 
+        case i `compare` i' of
+          EQ -> fail $ "Argument " ++ show i ++ " declared multiple times."
+          r -> r
   callDefine' False fn rreg (map snd (sortBy cmpFst (catMaybes args)))
 
   initPS <- fromMaybe (error "initializeVerification") <$> getPath
