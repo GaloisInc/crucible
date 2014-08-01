01cce3b97370bd5196997ddbe8252265e6f089a6
Aaron Tomb <atomb@galois.com>
2014-08-01 16:42:15 -0700

    Automatically find JDK core .jar file

--- src/SAWScript/Options.hs
+++ src/SAWScript/Options.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE CPP #-}
 module SAWScript.Options where
 
+import Data.List(isSuffixOf)
+import qualified Data.Set as Set
 import System.Console.GetOpt
 import System.Environment
+import System.Exit
 import System.FilePath
+import System.IO.Error
+import System.Process
 
 data Options = Options
   { importPath       :: [FilePath]
@@ -66,11 +71,23 @@ options =
 
 processEnv :: Options -> IO Options
 processEnv opts = do
-  env <- getEnvironment
-  return $ foldr addOpt opts env
+  curEnv <- getEnvironment
+  jars <- findJDKJar
+  return $ foldr addOpt (opts { jarList = jars ++ jarList opts }) curEnv
     where addOpt ("SAW_IMPORT_PATH", p) os =
             os { importPath = importPath os ++ splitSearchPath p }
           addOpt _ os = os
+
+findJDKJar :: IO [FilePath]
+findJDKJar = do
+  (_, out, _err) <- readProcessWithExitCode "java" ["-verbose"] ""
+                    `catchIOError`
+                    const (return (ExitFailure 1, "", ""))
+  let jars = filter isCoreJar . words . removeBrackets
+      removeBrackets = filter (not . (`elem` "[]"))
+      isCoreJar w = "rt.jar" `isSuffixOf` w ||
+                    "classes.jar" `isSuffixOf` w
+  return . Set.toList . Set.fromList . jars $ out
 
 pathDesc, pathDelim :: String
 
