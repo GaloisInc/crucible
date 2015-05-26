dc37968ad224715b3cdc295aae6ec4bf1025d460
Aaron Tomb <atomb@galois.com>
2015-05-26 08:33:40 -0700

    Update cleanup.sh to reflect current repo layout

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,14 @@
 (cd deps/abcBridge && rm -rf abc-build)
 (cd deps/abcBridge && cabal clean)
 (cd deps/aig && cabal clean)
+(cd deps/saw-core && cabal clean)
 (cd deps/cryptol && cabal clean)
+(cd deps/cryptol-verifier && cabal clean)
 (cd deps/jvm-parser && cabal clean)
+(cd deps/jvm-verifier && cabal clean)
 (cd deps/llvm-pretty && cabal clean)
 (cd deps/llvm-pretty-bc-parser && cabal clean)
-(cd ../Cryptol && cabal clean)
-(cd ../Java && cabal clean)
-(cd ../LLVM && cabal clean)
-(cd ../SAWCore && cabal clean)
+(cd deps/llvm-verifier && cabal clean)
 cabal clean
 rm -rf build
 rm -rf build-test
