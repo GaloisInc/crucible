40ba3208a102e6ae1367c9a8c541447e463e460a
Brian Huffman <huffman@galois.com>
2015-05-27 10:13:46 -0700

    Adapt to Cryptol's use of Data.Text.Lazy for parsing. Fixes #13.

--- src/SAWScript/CryptolEnv.hs
+++ src/SAWScript/CryptolEnv.hs
@@ -20,6 +20,7 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Data.Maybe (fromMaybe)
 import Data.Monoid
+import Data.Text.Lazy (Text, pack)
 import Data.Traversable
 
 import System.Environment.Executable(splitExecutablePath)
@@ -100,10 +101,14 @@ ioParseExpr :: Located String -> IO P.Expr
 ioParseExpr = ioParseGeneric P.parseExprWith
 
 ioParseDecls :: Located String -> IO [P.Decl]
-ioParseDecls = ioParseGeneric P.parseDeclsWith
+ioParseDecls = ioParseGeneric' P.parseDeclsWith
 
 ioParseSchema :: Located String -> IO P.Schema
-ioParseSchema = ioParseGeneric P.parseSchemaWith
+ioParseSchema = ioParseGeneric' P.parseSchemaWith
+
+ioParseGeneric' :: (P.Config -> Text -> Either P.ParseError a) -> Located String -> IO a
+ioParseGeneric' parse = ioParseGeneric parse'
+  where parse' cfg s = parse cfg (pack s)
 
 ioParseGeneric :: (P.Config -> String -> Either P.ParseError a) -> Located String -> IO a
 ioParseGeneric parse lstr = ioParseResult (parse cfg str)
