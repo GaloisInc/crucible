e7f5586c67e9b238ef5588f7e95b62d5b454b2e2
Nathan Collins <conathan@galois.com>
2015-05-27 14:51:33 -0700

    Add configurable dependency pinning.
    Also, update README a bit.
    
    The dependency pinning is disabled by default, but now we can create a
    `build-sandbox-version-pins.txt` file to pin dependencies, e.g. in a
    "stable" branch.

--- build-sandbox.sh
+++ build-sandbox.sh
@@ -47,12 +47,32 @@ fi
 PATH=${HERE}/${sandbox_dir}/bin:$PATH
 CABAL="cabal"
 
+# Pin a repo *if* a corresponding pin is defined in
+# './build-sandbox-version-pins.txt'. Assumes the CWD is in the
+# argument repo.
+#
+# The format of the pins file entries is '<repo> <committish>'. Lines
+# starting with '#' are treated as comments (because they aren't valid
+# repo names).
+pin () {
+  repo="$1"
+  echo Searching for pins for $repo ...
+  if [ -e "$HERE"/build-sandbox-version-pins.txt ] && \
+     grep "^$repo .\+\$" "$HERE"/build-sandbox-version-pins.txt &>/dev/null; then
+    echo Found pins\!
+    committish=$(sed -ne "s/^$repo \(.*\)\$/\1/p" < \
+      "$HERE"/build-sandbox-version-pins.txt)
+    echo Namely: $committish
+    git reset --hard "$committish"
+  fi
+}
+
 for repo in ${PUBLIC_GITHUB_REPOS} ; do
   if [ ! -e ./deps/${repo} ] ; then
     git clone https://github.com/GaloisInc/${repo}.git ./deps/${repo}
   fi
   if [ "${dopull}" == "true" ] ; then
-    (cd ./deps/${repo} && git checkout master && git pull)
+    (cd ./deps/${repo} && git checkout master && git pull && pin "$repo")
   fi
 done
 
@@ -61,7 +81,7 @@ for repo in ${PRIVATE_GITHUB_REPOS} ; do
     git clone git@github.com:GaloisInc/${repo}.git ./deps/${repo}
   fi
   if [ "${dopull}" == "true" ] ; then
-    (cd ./deps/${repo} && git checkout master && git pull)
+    (cd ./deps/${repo} && git checkout master && git pull && pin "$repo")
   fi
 done
 
