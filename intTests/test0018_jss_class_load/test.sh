4e6aae454f774e221aafa7bf7c8b86496d2cc8d2
Nathan Collins <conathan@galois.com>
2015-06-04 17:41:32 -0700

    Make the JSS class-loading test portable for Windows.
    I had used Unix search path separators.

--- intTests/test0018_jss_class_load/test.sh
+++ intTests/test0018_jss_class_load/test.sh
@@ -1,11 +1,13 @@
 set -e
 
-$JSS -c a:b org/example/Test
-$JSS -c a:b com/example/Test
+$JSS -c a${CPSEP}b org/example/Test
+$JSS -c a${CPSEP}b com/example/Test
 
 # These should timeout for JSS where
-# https://github.com/GaloisInc/jvm-verifier/issues/3 is not fixed.
-cp=$(pwd)/a:$(pwd)/b:/
+# https://github.com/GaloisInc/jvm-verifier/issues/3 is not fixed,
+# since JSS will attempt to load all '.class' files it can find at or
+# below the root directory.
+cp=$(pwd)${DIRSEP}a${CPSEP}$(pwd)${DIRSEP}b${CPSEP}.
 (cd / && $JSS -c "$cp" org/example/Test)
 (cd / && $JSS -c "$cp" com/example/Test)
 (cd / && $SAW -c "$cp" /dev/null)
