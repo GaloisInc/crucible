ea2cf32e85ae6a607d497f15d10522a9683a8a95
Nathan Collins <conathan@galois.com>
2015-06-05 10:34:35 -0700

    Make the JSS class-loading test portable for Windows?
    Round two: I forgot that '$JSS' includes an 'eval', so I need to
    quote/escape the semicolon search-path separator on Windows.

--- intTests/test0018_jss_class_load/test.sh
+++ intTests/test0018_jss_class_load/test.sh
@@ -1,13 +1,15 @@
 set -e
 
-$JSS -c a${CPSEP}b org/example/Test
-$JSS -c a${CPSEP}b com/example/Test
+# The '$JSS' command is defined to 'eval', so we need an extra level
+# of quoting to protect the semicolons in the classpath on Windows.
+$JSS -c "'a${CPSEP}b'" org/example/Test
+$JSS -c "'a${CPSEP}b'" com/example/Test
 
 # These should timeout for JSS where
 # https://github.com/GaloisInc/jvm-verifier/issues/3 is not fixed,
 # since JSS will attempt to load all '.class' files it can find at or
 # below the root directory.
 cp=$(pwd)${DIRSEP}a${CPSEP}$(pwd)${DIRSEP}b${CPSEP}.
-(cd / && $JSS -c "$cp" org/example/Test)
-(cd / && $JSS -c "$cp" com/example/Test)
-(cd / && $SAW -c "$cp" /dev/null)
+(cd / && $JSS -c "'$cp'" org/example/Test)
+(cd / && $JSS -c "'$cp'" com/example/Test)
+(cd / && $SAW -c "'$cp'" /dev/null)
