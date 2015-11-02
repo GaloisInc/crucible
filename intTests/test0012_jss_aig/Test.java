2b8513539e78c1f4cd42411a718e23fdadfcfd7b
Robert Dockins <rdockins@galois.com>
2015-11-02 11:04:50 -0800

    Update test0012 to use the new Symbolic.writeAiger() API These AIG files are now generated with explicitly-listed inputs. This fixes the mismatch which was previously causing this test to fail.
    Fixes #71

--- intTests/test0012_jss_aig/Test.java
+++ intTests/test0012_jss_aig/Test.java
@@ -3,12 +3,12 @@ class Test {
     public static void main(String[] args) {
         byte x = Symbolic.freshByte((byte)0);
         byte y = Symbolic.freshByte((byte)0);
-        Symbolic.writeAiger("tmp/x__x.aig", x == x);
-        Symbolic.writeAiger("tmp/x__x1.aig", x == x + 1);
-        Symbolic.writeAiger("tmp/x__y.aig", x == y);
-        Symbolic.writeAiger("tmp/xx.aig", x + x);
-        Symbolic.writeAiger("tmp/2x.aig", x * 2);
-        Symbolic.writeAiger("tmp/yy.aig", y + y);
-        Symbolic.writeAiger("tmp/2y.aig", y * 2);
+        Symbolic.writeAiger("tmp/x__x.aig" , x == x      , x, y );
+        Symbolic.writeAiger("tmp/x__x1.aig", x == x + 1  , x, y );
+        Symbolic.writeAiger("tmp/x__y.aig" , x == y      , x, y );
+        Symbolic.writeAiger("tmp/xx.aig"   , x + x       , x, y );
+        Symbolic.writeAiger("tmp/2x.aig"   , x * 2       , x, y );
+        Symbolic.writeAiger("tmp/yy.aig"   , y + y       , x, y );
+        Symbolic.writeAiger("tmp/2y.aig"   , y * 2       , x, y );
     }
 }
