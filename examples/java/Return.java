53caf6ab1535c86193ffefc075a29eb1927ecb86
Aaron Tomb <atomb@galois.com>
2015-11-04 08:49:17 -0800

    Update example of Java array returns
    Specifying fields of newly allocated objects still incomplete.

--- examples/java/Return.java
+++ examples/java/Return.java
@@ -5,6 +5,13 @@ class Return {
         return x;
     }
 
+    public static SimpleObj newSimple(int x, int y) {
+        SimpleObj o = new SimpleObj();
+        o.x = x;
+        o.y = y;
+        return o;
+    }
+
     public static void main(String[] args) {
         System.out.println(fill(22)[3]);
     }
