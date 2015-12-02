f99452b70fefc5f9535c54518b05160bd548eaa0
Aaron Tomb <aarontomb@gmail.com>
2015-12-02 15:56:45 -0800

    Additional examples of structs with `llvm_symexec`

--- examples/llvm/struct.c
+++ examples/llvm/struct.c
@@ -2,12 +2,15 @@ typedef struct {
     int *x;
 } s;
 
-/*
-int add_direct(s o) {
-    return o.x + o.y;
-}
-*/
-
 int add_indirect(s *o) {
     return (o->x)[0] + (o->x)[1];
+}
+
+void set_indirect(s *o) {
+    (o->x)[0] = 0;
+    (o->x)[1] = 0;
+}
+
+s *s_id(s *o) {
+    return o;
 }
