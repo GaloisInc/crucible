c359ee62158401ecec1f47cb0d89aac32caba038
Aaron Tomb <atomb@galois.com>
2016-02-29 15:21:41 -0800

    Additional comments for AES example

--- examples/openssl_aes/aes_wrap.c
+++ examples/openssl_aes/aes_wrap.c
@@ -1,7 +1,11 @@
 #include <aes.h>
 #include <sym-api.h>
 
-int aes_encrypt(const unsigned char *in, unsigned char *out, const unsigned char *key) {
+// A wrapper to make OpenSSL's AES work in one pass, like the Cryptol
+// specification.
+int aes_encrypt(const unsigned char *in,
+                unsigned char *out,
+                const unsigned char *key) {
   AES_KEY rkey;
   int r = private_AES_set_encrypt_key(key, 128, &rkey);
   AES_encrypt(in, out, &rkey);
@@ -9,9 +13,19 @@ int aes_encrypt(const unsigned char *in, unsigned char *out, const unsigned char
 }
 
 int main() {
+
+    // There are two fresh, unconstrained inputs: the plain text and the
+    // key.
     unsigned char *in = lss_fresh_array_uint8(16, 0, NULL);
     unsigned char *key = lss_fresh_array_uint8(16, 0, NULL);
+
+    // The output is written to space allocated by the caller.
     unsigned char *out = malloc(16 * sizeof(unsigned char));
+
+    // Run the encryption.
     aes_encrypt(in, out, key);
+
+    // Write the symbolic representation of the result into an
+    // And-Inverter Graph.
     lss_write_aiger_array_uint8(out, 16, "aes_imp.aig");
 }
