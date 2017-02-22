f344ea290dfe5ffc7a0aab4dbf996ed96989e093
Andrey Chudnov <andrey@chudnov.com>
2017-02-21 17:18:24 -0800

    WIP translation Go -> Crucible

---
+++ go-crucible/Main.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Lang.Crucible.Go.Translation
+import Language.Go.Parser
+import Language.Go.AST
+import Control.Monad.ST
+import Lang.Crucible.Core
+
+main :: IO ()
+main = case runST $ translateFunction (Id undefined "f") undefined undefined [] of
+         AnyCFG cfg -> putStrLn $ show cfg
+
