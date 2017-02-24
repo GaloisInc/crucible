0831d8c16ad88fe9fb097700fca3dea97fccd09f
Andrey Chudnov <andrey@chudnov.com>
2017-02-23 19:23:18 -0800

    Added comments. Continuing laying down the translation structure.

--- go-crucible/src/Lang/Crucible/Go/Translation.hs
+++ go-crucible/src/Lang/Crucible/Go/Translation.hs
@@ -5,7 +5,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GADTs #-}
 
-module Lang.Crucible.Go.Translation where
+module Lang.Crucible.Go.Translation (translateFunction) where
 
 import Language.Go.AST
 import Language.Go.Parser (ParserAnnotation)
@@ -38,8 +38,14 @@ import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import Data.Default.Class
 import Lens.Simple
-  
-translateFunction :: forall h. Id ParserAnnotation -> ParameterList ParserAnnotation -> ReturnList ParserAnnotation -> [Statement ParserAnnotation] -> ST h AnyCFG
+
+-- | (Currently) the entry point of the module: translates one go
+-- function to a Crucible control-flow graph. The parameters are the
+-- same as for the `FunctionDecl` AST constructor.
+translateFunction :: forall h. Id ParserAnnotation -- ^ Function name
+                  -> ParameterList ParserAnnotation -- ^ Function parameters
+                  -> ReturnList ParserAnnotation
+                  -> [Statement ParserAnnotation] -> ST h AnyCFG
 translateFunction (Id _ fname) params returns body =
   withHandleAllocator $ \ha ->
   bindReturns returns $ \(retctx :: CtxRepr tretctx) setupReturns ->
@@ -55,6 +61,11 @@ translateFunction (Id _ fname) params returns body =
      case toSSA g of
        C.SomeCFG gs -> return $ AnyCFG gs
 
+-- | Bind the list of return values to both the function return type
+-- in the crucible CFG and, if the return values are named, to
+-- crucible registers mapped to the names. Like many functions here,
+-- uses, continuation-passing style to construct heterogeneous lists
+-- and work with type-level literals.
 bindReturns :: ReturnList ParserAnnotation
             -> (forall ctx. CtxRepr ctx -> (forall s ret rctx. Generator h s (TransState rctx) ret (Maybe (VariableAssignment s ctx))) -> a)
             -> a
@@ -81,11 +92,19 @@ bindReturns rlist f =
     NamedReturnList _ nrets -> goNamed nrets $ \ctx gen -> f ctx (Just <$> gen)
     AnonymousReturnList _ rtypes -> goAnon rtypes $ \ctx -> f ctx (return Nothing)
 
+
 type VariableAssignment s ctx = Ctx.Assignment (GoVarOpen s) ctx
+
+-- | GoVarReg and GoVarOpen are respectively the closed (abstracting
+-- from type parameters) and open representations of Crucible
+-- registers that store the value of a variable with a given type.
+
 newtype GoVarOpen s tp = GoVarOpen {unGoVarOpen :: Reg s (ReferenceType tp)}
+data GoVarReg s where
+  GoVarReg :: TypeRepr tp -> Reg s (ReferenceType tp) -> GoVarReg s
 
 -- | Generate the Crucible type context and bind parameter names to
--- (typed) Crucible registers
+-- (typed) Crucible registers.
 bindParams :: ParameterList ParserAnnotation
            -> (forall ctx. CtxRepr ctx
                -> (forall s ret. Ctx.Assignment (Atom s) ctx -> Generator h s (TransState rctx) ret ())
@@ -107,18 +126,24 @@ bindParams plist f =
     NamedParameterList _ params mspread -> go (params ++ maybeToList mspread) f
     AnonymousParameterList _ _ _ -> error "Unexpected anonymous parameter list in a function definition"
 
-data TransState ctx s = TransState {lexenv :: !(HashMap Text (GoVarReg s))
-                                   ,enclosingLabels :: !(LabelStack s)
-                                   ,returnSlots :: !(Maybe (VariableAssignment s ctx))
-                                   }
+-- | State of translation: the user state part of the Generator monad used here.
+data TransState ctx s = TransState
+  {lexenv :: !(HashMap Text (GoVarReg s)) -- ^ A mapping from variable names to registers
+  ,enclosingLabels :: !(LabelStack s) -- ^ Labels lexically enclosing current source location
+  ,returnSlots :: !(Maybe (VariableAssignment s ctx))
+   -- ^ The list of registers that represent the components of the
+   -- return value. Crucible functions can only have one return value,
+   -- while Go functions can have multiple. To deal with that we
+   -- multiplex return values in a struct that would store references
+   -- to return values. The struct is going to be created from the
+   -- variable assignment in this component of the user state.
+  }
 
 instance Default (TransState ctx s) where
   def = TransState {lexenv = HM.empty
                    ,enclosingLabels = LabelStack []
                    ,returnSlots = Nothing}
 
-data GoVarReg s where
-  GoVarReg :: TypeRepr tp -> Reg s (ReferenceType tp) -> GoVarReg s
                    
 newtype LabelStack s = LabelStack [Gen.Label s]
 
@@ -128,18 +153,41 @@ declareVar name value t = do ref <- newReference value
                              modify' (\ts -> ts {lexenv = HM.insert name (GoVarReg t reg) (lexenv ts)})
                              return reg
 
-
 graphGenerator :: [Statement ParserAnnotation] -> TypeRepr ret -> Generator h s (TransState rctx) ret (Expr s ret)
 graphGenerator body retTypeRepr =
-  do mapM (\s -> generateStatement s retTypeRepr) body
+  do rets <- liftM catMaybes $ mapM (\s -> translateStatement s retTypeRepr) body
+     -- The following is going to be a fallthrough block that would
+     -- never be reachable if all the exit paths in the function graph
+     -- end with a return statement.
      reportError $ App (C.TextLit "Expected a return statement, but found none.")
   -- do Just Refl <- return $ testEquality retTypeRepr $ BVRepr (knownNat :: NatRepr 32)
   --    return $ App (C.BVLit (knownNat :: NatRepr 32) 12)
 
-generateStatement :: Statement ParserAnnotation -> TypeRepr ret -> Generator h s (TransState rctx) ret (Expr s ret)
-generateStatement s retTypeRepr = undefined
+-- | Translates individual statements. This is where Go statement semantics is encoded.
+translateStatement :: Statement ParserAnnotation -> TypeRepr ret -> Generator h s (TransState rctx) ret (Maybe (Expr s ret))
+translateStatement s retTypeRepr = case s of
+  DeclStmt _ (VarDecl _ varspecs)     -> mapM_ translateVarSpec varspecs >> return Nothing
+  DeclStmt _ (ConstDecl _ constspecs) -> error $ "Unsupported Go statement " ++ show s
+  DeclStmt _ (TypeDecl _ _) ->
+    -- type declarations are only reflected in type analysis results
+    -- and type translations; they are not executable
+    return Nothing 
+    -- | The number of expressions (|e|) in `lhs` and `rhs` should
+    -- either be the same, or |rhs| = 1. Assigning multi-valued
+    -- right-hand sides (|rhs|=1 and |lhs| > 1) is not currently
+    -- supported.
+  AssignStmt _ lhs op rhs -> error $ "Unsupported Go statement " ++ show s
+  _ -> error $ "Unsupported Go statement " ++ show s
 
--- | Translate a Go type to a Crucible type
+translateVarSpec :: VarSpec ParserAnnotation -> Generator h s (TransState rctx) ret ()
+translateVarSpec s = case s of
+  -- the rules for matching multiple variables and expressions are the
+  -- same as for normal expressions. We don't support multi-valued right-hand-sides yet.
+  TypedVarSpec _ identifiers typ initialValues -> undefined
+  UntypedVarSpec _ identifiers initialValues -> error "Untyped variable declarations will be supported in V4"
+  
+  
+-- | Translate a Go type to a Crucible type. This is where type semantics is encoded.
 translateType :: forall a. Int32 {-Target architecture word size-}
               -> ValueType
               -> (forall typ. TypeRepr typ -> (forall s. Expr s typ) -> a)
