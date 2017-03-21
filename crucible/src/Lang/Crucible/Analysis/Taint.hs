ce8e2cdd98a21a11afe4b2753510517640dd10c1
Andrey Chudnov <andrey@chudnov.com>
2017-03-21 09:38:22 -0700

    Simple taint domain

--- crucible/src/Lang/Crucible/Analysis/Taint.hs
+++ crucible/src/Lang/Crucible/Analysis/Taint.hs
@@ -1,20 +1,24 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+
 module Lang.Crucible.Analysis.Taint where
 
 import Lang.Crucible.Analysis.Fixpoint
+import Lang.Crucible.Core
 
---data Taint = Tainted | Untainted
+data Tainted (tp :: CrucibleType) where
+  Tainted :: Tainted tp
+  Untainted :: Tainted tp
+  deriving (Eq, Show)
 
--- type family Tainted a
--- type instance T
+taintDomain :: Domain Tainted
+taintDomain = Domain {domTop = Tainted
+                     ,domBottom = Untainted
+                     ,domJoin = \t1 t2 -> if t1 == Tainted || t2 == Tainted then Tainted
+                                          else Untainted
+                     ,domEq = (==)
+                     ,domIter = WTO
+                     }
 
--- data Tainted = forall (a :: CrucibleType). Tainted | Untainted
-
--- taintDomain = Domain {domTop = Untainted
---                      ,domBottom = Tainted
---                      ,domJoin = \t1 t2 -> case (t1, t2) of
---                          (Tainted, _) -> Tainted
---                          (_, Tainted) -> Tainted
---                          _            -> Untainted
---                      } 
