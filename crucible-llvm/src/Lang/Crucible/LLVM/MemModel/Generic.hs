GaloisInc/crucible

commit e242ec3b9e97f5015f7a06a8284fc42bd3693c76

Fix exponential run-time behavior in `readMem`.
Fixes GaloisInc/saw-script#198.

crucible-llvm/src/Lang/Crucible/LLVM/MemModel/Generic.hs
@@ -293,7 +293,6 @@ readMem sym w l tp m = do
 -- read (which may be anything if read was unsuccessful).
 readMem' :: forall w sym . (1 <= w, IsSymInterface sym)
          => sym -> NatRepr w
-            -- ^ Functions for primitive operations on addresses, propositions, and values.
          -> (LLVMPtr sym w, AddrDecomposeResult sym w)
             -- ^ Address we are reading along with information about how it was constructed.
          -> Type
@@ -301,27 +300,37 @@ readMem' :: forall w sym . (1 <= w, IsSymInterface sym)
          -> [MemWrite sym w]
             -- ^ List of writes.
          -> IO (Pred sym, PartLLVMVal sym w)
-readMem' sym _ _ tp [] = badLoad sym tp
-readMem' sym w l tp (h:r) = do
-  cache <- newIORef Map.empty
-  let readPrev :: Type -> (LLVMPtr sym w, AddrDecomposeResult sym w) -> IO (Pred sym, PartLLVMVal sym w)
-      readPrev tp' l' = do
-        m <- readIORef cache
-        case Map.lookup (tp',fst l') m of
-          Just x -> return x
-          Nothing -> do
-            x <- readMem' sym w l' tp' r
-            writeIORef cache $ Map.insert (tp',fst l') x m
-            return x
-  case h of
-    MemCopy dst src sz -> do
-      readMemCopy sym w l tp dst src sz readPrev
-    MemStore dst v stp -> do
-      readMemStore sym w l tp dst v stp readPrev
-    WriteMerge c xr yr -> do
-      join $ tgMuxPair sym w c
-               <$> readMem' sym w l tp (xr++r)
-               <*> readMem' sym w l tp (yr++r)
+readMem' sym w l0 tp0 = go (badLoad sym tp0) l0 tp0
+  where
+    go :: IO (Pred sym, PartLLVMVal sym w) ->
+          (LLVMPtr sym w, AddrDecomposeResult sym w) ->
+          Type ->
+          [MemWrite sym w] ->
+          IO (Pred sym, PartLLVMVal sym w)
+    go fallback _ _ [] = fallback
+    go fallback l tp (h : r) =
+      do cache <- newIORef Map.empty
+         let readPrev :: Type -> (LLVMPtr sym w, AddrDecomposeResult sym w) -> IO (Pred sym, PartLLVMVal sym w)
+             readPrev tp' l' = do
+               m <- readIORef cache
+               case Map.lookup (tp',fst l') m of
+                 Just x -> return x
+                 Nothing -> do
+                   x <- go fallback l' tp' r
+                   writeIORef cache $ Map.insert (tp',fst l') x m
+                   return x
+         case h of
+           MemCopy dst src sz ->
+             readMemCopy sym w l tp dst src sz readPrev
+           MemStore dst v stp ->
+             readMemStore sym w l tp dst v stp readPrev
+           WriteMerge _ [] [] ->
+             go fallback l tp r
+           WriteMerge c xr yr ->
+             do p <- go fallback l tp r -- TODO: wrap this in a delay
+                x <- go (return p) l tp xr
+                y <- go (return p) l tp yr
+                tgMuxPair sym w c x y
 
 -- | A state of memory as of a stack push, branch, or merge.
 data MemState d =

commit ec511cc02406de1d530895e74f9c14c01898ea03

Fix memory model bug arising when conditional write covers part of a value.
The bug was introduced in revision e242ec3b. The problem was that if you
are reading a large value (e.g. a struct) and a conditional write covers
only part of the range you are reading, then you need to read the *other*
part from the old memory. However the only value we cached was the value
of the *entire struct* read from the old memory. Therefore we end up with
a malformed value that doesn't even have the correct type.

Now, instead of caching a single value, we have a memo table indexed by
type and offset, so we can return the appropriate sub-value. Hopefully
we still avoid the exponential run-time behavior that we had before
e242ec3b.

crucible-llvm/src/Lang/Crucible/LLVM/MemModel/Generic.hs
@@ -428,14 +428,14 @@ readMem' :: forall w sym . (1 <= w, IsSymInterface sym)
          -> [MemWrite sym]
             -- ^ List of writes.
          -> IO (PartLLVMVal sym)
-readMem' sym w l0 tp0 alignment = go (badLoad sym tp0) l0 tp0
+readMem' sym w l0 tp0 alignment = go (\tp _l -> badLoad sym tp) l0 tp0
   where
-    go :: IO (PartLLVMVal sym) ->
+    go :: (Type -> (LLVMPtr sym w, AddrDecomposeResult sym w) -> IO (PartLLVMVal sym)) ->
           (LLVMPtr sym w, AddrDecomposeResult sym w) ->
           Type ->
           [MemWrite sym] ->
           IO (PartLLVMVal sym)
-    go fallback _ _ [] = fallback
+    go fallback l tp [] = fallback tp l
     go fallback l tp (h : r) =
       do cache <- newIORef Map.empty
          let readPrev :: Type -> (LLVMPtr sym w, AddrDecomposeResult sym w) -> IO (PartLLVMVal sym)
@@ -459,9 +459,8 @@ readMem' sym w l0 tp0 alignment = go (badLoad sym tp0) l0 tp0
            WriteMerge _ [] [] ->
              go fallback l tp r
            WriteMerge c xr yr ->
-             do p <- go fallback l tp r -- TODO: wrap this in a delay
-                x <- go (return p) l tp xr
-                y <- go (return p) l tp yr
+             do x <- go readPrev l tp xr
+                y <- go readPrev l tp yr
                 muxLLVMVal sym c x y
 
 --------------------------------------------------------------------------------