c3b378ab99a7e90810551cfea19416217c169c7a
Iavor Diatchki <iavor.diatchki@gmail.com>
2018-05-17 09:09:30 -0700

    Remove unused `sym` parameter from `BranchResult`

--- crucible/src/Lang/Crucible/Backend.hs
+++ crucible/src/Lang/Crucible/Backend.hs
@@ -82,7 +82,7 @@ type AssumptionState sym = AS.AssumptionStack (Pred sym) AssumptionReason SimErr
 
 
 -- | Result of attempting to branch on a predicate.
-data BranchResult sym
+data BranchResult
      -- | Branch is symbolic.
      --
      -- The Boolean value indicates whether the backend suggests that the active
@@ -111,7 +111,7 @@ class IsBoolSolver sym where
   --   this decides what the next course of action should be for the branch.
   evalBranch :: sym
              -> Pred sym -- Predicate to branch on.
-             -> IO (BranchResult sym)
+             -> IO BranchResult
 
   -- | Push a new assumption frame onto the stack.  Assumptions and assertions
   --   made will now be associated with this frame on the stack until a new
