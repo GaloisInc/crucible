GaloisInc/crucible

commit fb89069eb803c9479fca9b4d3ecad97fd6cce937

Some updates to the What4.Partial module, including a new `mergePartials` operation.  Some minor changes to the `mergePartial` function require updates in crucible-llvm.

crucible-llvm/src/Lang/Crucible/LLVM/MemModel/Pointer.hs
@@ -516,31 +516,31 @@ muxLLVMVal :: forall sym
    -> PartLLVMVal sym
    -> PartLLVMVal sym
    -> IO (PartLLVMVal sym)
-muxLLVMVal sym p = mergePartial sym muxval p
+muxLLVMVal sym p0 = mergePartial sym muxval p0
   where
-    muxval :: LLVMVal sym -> LLVMVal sym -> PartialT sym IO (LLVMVal sym)
+    muxval :: Pred sym -> LLVMVal sym -> LLVMVal sym -> PartialT sym IO (LLVMVal sym)
 
-    muxval (LLVMValInt base1 off1) (LLVMValInt base2 off2)
+    muxval p (LLVMValInt base1 off1) (LLVMValInt base2 off2)
       | Just Refl <- testEquality (bvWidth off1) (bvWidth off2)
       = do base <- liftIO $ natIte sym p base1 base2
            off  <- liftIO $ bvIte sym p off1 off2
            return $ LLVMValInt base off
 
-    muxval (LLVMValReal xsz x) (LLVMValReal ysz y) | xsz == ysz =
+    muxval p (LLVMValReal xsz x) (LLVMValReal ysz y) | xsz == ysz =
       do z <- liftIO $ realIte sym p x y
          return $ LLVMValReal xsz z
 
-    muxval (LLVMValStruct fls1) (LLVMValStruct fls2)
+    muxval p (LLVMValStruct fls1) (LLVMValStruct fls2)
       | fmap fst fls1 == fmap fst fls2 = do
-          fls <- traverse id $ V.zipWith (\(f,x) (_,y) -> (f,) <$> muxval x y) fls1 fls2
+          fls <- traverse id $ V.zipWith (\(f,x) (_,y) -> (f,) <$> muxval p x y) fls1 fls2
           return $ LLVMValStruct fls
 
-    muxval (LLVMValArray tp1 v1) (LLVMValArray tp2 v2)
+    muxval p (LLVMValArray tp1 v1) (LLVMValArray tp2 v2)
       | tp1 == tp2 && V.length v1 == V.length v2 = do
-          v <- traverse id $ V.zipWith muxval v1 v2
+          v <- traverse id $ V.zipWith (muxval p) v1 v2
           return $ LLVMValArray tp1 v
 
-    muxval _ _ = returnUnassigned
+    muxval _ _ _ = returnUnassigned
 
 
 ppPtr :: IsExpr (SymExpr sym) => LLVMPtr sym wptr -> Doc

what4/src/What4/Partial.hs
@@ -21,13 +21,14 @@ module What4.Partial
  , maybePartExpr
  , joinMaybePE
    -- * PartialT
- , PartialT
+ , PartialT(..)
  , runPartialT
  , returnUnassigned
  , returnMaybe
  , returnPartial
  , addCondition
  , mergePartial
+ , mergePartials
  ) where
 
 import Control.Monad.IO.Class
@@ -69,18 +70,19 @@ joinMaybePE (Just pe) = pe
 ------------------------------------------------------------------------
 -- Merge
 
+-- | If-then-else on partial expressions.
 mergePartial :: (IsExprBuilder sym, MonadIO m) =>
   sym ->
-  (a -> a -> PartialT sym m a) ->
-  Pred sym ->
-  PartExpr (Pred sym) a ->
-  PartExpr (Pred sym) a ->
+  (Pred sym -> a -> a -> PartialT sym m a) {- ^ Operation to combine inner values -} ->
+  Pred sym {- ^ condition to merge on -} ->
+  PartExpr (Pred sym) a {- ^ 'if' value -}  ->
+  PartExpr (Pred sym) a {- ^ 'then' value -} ->
   m (PartExpr (Pred sym) a)
 
 {-# SPECIALIZE mergePartial ::
       IsExprBuilder sym =>
       sym ->
-      (a -> a -> PartialT sym IO a) ->
+      (Pred sym -> a -> a -> PartialT sym IO a) ->
       Pred sym ->
       PartExpr (Pred sym) a ->
       PartExpr (Pred sym) a ->
@@ -89,12 +91,30 @@ mergePartial :: (IsExprBuilder sym, MonadIO m) =>
 mergePartial _ _ _ Unassigned Unassigned =
      return Unassigned
 mergePartial sym _ c (PE px x) Unassigned =
-     PE <$> liftIO (andPred sym px c) <*> return x
+     do p <- liftIO $ andPred sym px c
+        return $! mkPE p x
 mergePartial sym _ c Unassigned (PE py y) =
-     PE <$> liftIO (andPred sym py =<< notPred sym c) <*> return y
+     do p <- liftIO (andPred sym py =<< notPred sym c)
+        return $! mkPE p y
 mergePartial sym f c (PE px x) (PE py y) =
-  do p <- liftIO (itePred sym c px py)
-     runPartialT sym p (f x y)
+    do p <- liftIO (itePred sym c px py)
+       runPartialT sym p (f p x y)
+
+-- | Merge a collection of partial values in an if-then-else tree.
+--   For example, if we merge a list like @[(xp,x),(yp,y),(zp,z)]@,
+--   we get a value that is morally equivalant to:
+--   @if xp then x else (if yp then y else (if zp then z else undefined))@.
+mergePartials :: (IsExprBuilder sym, MonadIO m) =>
+  sym ->
+  (Pred sym -> a -> a -> PartialT sym m a) {- ^ Operation to combine inner values -} ->
+  [(Pred sym, PartExpr (Pred sym) a)]      {- ^ values to merge -} ->
+  m (PartExpr (Pred sym) a)
+mergePartials sym f = go
+  where
+  go [] = return Unassigned
+  go ((c,x):xs) =
+    do y <- go xs
+       mergePartial sym f c x y
 
 ------------------------------------------------------------------------
 -- PartialT
@@ -119,11 +139,11 @@ instance Functor m => Functor (PartialT sym m) where
 -- We depend on the monad transformer as partialT explicitly orders
 -- the calls to the functions in (<*>).  This ordering allows us to
 -- avoid having any requirements that sym implement a partial interface.
-instance Monad m => Applicative (PartialT sym m) where
-  pure a = PartialT $ \_ p -> pure $! PE p a
+instance (IsExpr (SymExpr sym), Monad m) => Applicative (PartialT sym m) where
+  pure a = PartialT $ \_ p -> pure $! mkPE p a
   mf <*> mx = mf >>= \f -> mx >>= \x -> pure (f x)
 
-instance Monad m => Monad (PartialT sym m) where
+instance (IsExpr (SymExpr sym), Monad m) => Monad (PartialT sym m) where
   return = pure
   m >>= h =
     PartialT $ \sym p -> do
@@ -136,7 +156,7 @@ instance Monad m => Monad (PartialT sym m) where
 instance MonadTrans (PartialT sym) where
   lift m = PartialT $ \_ p -> PE p <$> m
 
-instance MonadIO m => MonadIO (PartialT sym m) where
+instance (IsExpr (SymExpr sym), MonadIO m) => MonadIO (PartialT sym m) where
   liftIO = lift . liftIO
 
 -- | End the partial computation and just return the unassigned value.
@@ -144,9 +164,9 @@ returnUnassigned :: Applicative m => PartialT sym m a
 returnUnassigned = PartialT $ \_ _ -> pure Unassigned
 
 -- | Lift a 'Maybe' value to a partial expression.
-returnMaybe :: Applicative m =>  Maybe a -> PartialT sym m a
+returnMaybe :: (IsExpr (SymExpr sym), Applicative m) =>  Maybe a -> PartialT sym m a
 returnMaybe Nothing  = returnUnassigned
-returnMaybe (Just a) = PartialT $ \_ p -> pure (PE p a)
+returnMaybe (Just a) = PartialT $ \_ p -> pure (mkPE p a)
 
 -- | Return a partial expression.
 --
@@ -156,16 +176,10 @@ returnPartial :: (IsExprBuilder sym, MonadIO m)
               => PartExpr (Pred sym) a
               -> PartialT sym m a
 returnPartial Unassigned = returnUnassigned
-returnPartial (PE q a) =
-    case asConstantPred q of
-      Just False -> returnUnassigned
-      _ -> PartialT $ \sym p -> liftIO $ resolve <$> andPred sym p q
-  where resolve r = case asConstantPred r of
-                      Just False -> Unassigned
-                      _ -> PE r a
+returnPartial (PE q a) = PartialT $ \sym p -> liftIO (mkPE <$> andPred sym p q <*> pure a)
 
 -- | Add an extra condition to the current partial computation.
 addCondition :: (IsExprBuilder sym, MonadIO m)
               => Pred sym
               -> PartialT sym m ()
-addCondition q = returnPartial (PE q ())
+addCondition q = returnPartial (mkPE q ())

commit 52412d2e515aa5c278f462e4107368afa4616dbd

Fix bug introduced in fb89069.
Function `muxLLVMVal` had been modified to adapt to changes in
`mergePartial`. However, it was modified in the wrong way: the
new parameter to the callback function `muxval` is a definedness
predicate, but `muxval` was using it as if it were the branch
condition.

Fixes GaloisInc/saw-script#287.

crucible-llvm/src/Lang/Crucible/LLVM/MemModel/Pointer.hs
@@ -516,31 +516,31 @@ muxLLVMVal :: forall sym
    -> PartLLVMVal sym
    -> PartLLVMVal sym
    -> IO (PartLLVMVal sym)
-muxLLVMVal sym p0 = mergePartial sym muxval p0
+muxLLVMVal sym cond = mergePartial sym (const muxval) cond
   where
-    muxval :: Pred sym -> LLVMVal sym -> LLVMVal sym -> PartialT sym IO (LLVMVal sym)
+    muxval :: LLVMVal sym -> LLVMVal sym -> PartialT sym IO (LLVMVal sym)
 
-    muxval p (LLVMValInt base1 off1) (LLVMValInt base2 off2)
+    muxval (LLVMValInt base1 off1) (LLVMValInt base2 off2)
       | Just Refl <- testEquality (bvWidth off1) (bvWidth off2)
-      = do base <- liftIO $ natIte sym p base1 base2
-           off  <- liftIO $ bvIte sym p off1 off2
+      = do base <- liftIO $ natIte sym cond base1 base2
+           off  <- liftIO $ bvIte sym cond off1 off2
            return $ LLVMValInt base off
 
-    muxval p (LLVMValReal xsz x) (LLVMValReal ysz y) | xsz == ysz =
-      do z <- liftIO $ realIte sym p x y
+    muxval (LLVMValReal xsz x) (LLVMValReal ysz y) | xsz == ysz =
+      do z <- liftIO $ realIte sym cond x y
          return $ LLVMValReal xsz z
 
-    muxval p (LLVMValStruct fls1) (LLVMValStruct fls2)
+    muxval (LLVMValStruct fls1) (LLVMValStruct fls2)
       | fmap fst fls1 == fmap fst fls2 = do
-          fls <- traverse id $ V.zipWith (\(f,x) (_,y) -> (f,) <$> muxval p x y) fls1 fls2
+          fls <- traverse id $ V.zipWith (\(f,x) (_,y) -> (f,) <$> muxval x y) fls1 fls2
           return $ LLVMValStruct fls
 
-    muxval p (LLVMValArray tp1 v1) (LLVMValArray tp2 v2)
+    muxval (LLVMValArray tp1 v1) (LLVMValArray tp2 v2)
       | tp1 == tp2 && V.length v1 == V.length v2 = do
-          v <- traverse id $ V.zipWith (muxval p) v1 v2
+          v <- traverse id $ V.zipWith muxval v1 v2
           return $ LLVMValArray tp1 v
 
-    muxval _ _ _ = returnUnassigned
+    muxval _ _ = returnUnassigned
 
 
 ppPtr :: IsExpr (SymExpr sym) => LLVMPtr sym wptr -> Doc

what4/src/What4/Partial.hs
@@ -73,7 +73,9 @@ joinMaybePE (Just pe) = pe
 -- | If-then-else on partial expressions.
 mergePartial :: (IsExprBuilder sym, MonadIO m) =>
   sym ->
-  (Pred sym -> a -> a -> PartialT sym m a) {- ^ Operation to combine inner values -} ->
+  (Pred sym -> a -> a -> PartialT sym m a)
+    {- ^ Operation to combine inner values. The 'Pred' parameter is the
+         definedness predicate for the resulting value. -} ->
   Pred sym {- ^ condition to merge on -} ->
   PartExpr (Pred sym) a {- ^ 'if' value -}  ->
   PartExpr (Pred sym) a {- ^ 'then' value -} ->