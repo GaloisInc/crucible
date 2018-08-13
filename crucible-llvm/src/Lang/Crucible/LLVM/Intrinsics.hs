521dffc1216b867588b5d3c83c762b8381325d1c
Rob Dockins <rdockins@galois.com>
2018-08-13 13:37:52 -0700

    Silence unused varaible warning

--- crucible-llvm/src/Lang/Crucible/LLVM/Intrinsics.hs
+++ crucible-llvm/src/Lang/Crucible/LLVM/Intrinsics.hs
@@ -1207,7 +1207,7 @@ printfOps sym valist =
 
   , printfGetFloat = \i _len ->
      case valist V.!? (i-1) of
-       Just (AnyValue (FloatRepr _fi) x) ->
+       Just (AnyValue (FloatRepr _fi) _x) ->
          -- TODO: handle interpreted floats
          return Nothing
        Just (AnyValue tpr _) ->
