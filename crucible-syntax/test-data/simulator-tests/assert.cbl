7f03c09737158283261cf7cb0bfe376b35825019
Rob Dockins <rdockins@galois.com>
2018-08-16 16:03:28 -0700

    Additional simulator tests

---
+++ crucible-syntax/test-data/simulator-tests/assert.cbl
@@ -0,0 +1,5 @@
+(defun @main () Unit
+  (start start:
+    (let x (fresh Bool))
+    (assert! x "x is true")
+    (return ())))
---
+++ crucible-syntax/test-data/simulator-tests/assume.cbl
@@ -0,0 +1,5 @@
+(defun @main () Unit
+  (start start:
+    (let x (fresh Bool))
+    (assume! x "x is true")
+    (error "error statement")))
