2641d2cb330e96571aba54f071806ba4ad791ee4
Stephanie Weirich <sweirich@galois.com>
2018-09-12 09:34:59 -0400

    Strange failing test case with double arithmetic

---
+++ crucible-jvm/tests/DoubleSum.java
@@ -0,0 +1,19 @@
+public class DoubleSum
+{
+    // 
+    public static void main(String[] args)
+    {
+	int i2 = 2;
+	double sum = (3.4 + i2);
+	System.out.println(sum);
+
+	if (sum != 5.4) {
+	    System.out.println("FAIL");
+	}
+	else {
+	    System.out.println("PASS");
+      }	
+    }
+    
+ 
+}
