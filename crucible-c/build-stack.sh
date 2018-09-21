2a74b736307a5be02a6f18818547ec2946d0a7c3
Aaron Tomb <atomb@galois.com>
2018-09-21 13:30:39 -0700

    Adjust use of STACK_YAML for crucible-c

--- crucible-c/build-stack.sh
+++ crucible-c/build-stack.sh
@@ -1,5 +1,6 @@
 (cd .. && GITHUB_URL="https://github.com/" NO_GIT_PULL="true" ./scripts/build-sandbox.sh)
-STACK_YAML=stack-ghc-8.2.yaml stack build
+export STACK_YAML=stack-ghc-8.2.yaml
+stack build
 # Link bin directory to a more convenient location
 rm -f bin
 ln -s `stack path --local-install-root`/bin
