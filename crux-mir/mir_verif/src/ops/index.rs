6bc06bde1483c7f08ef9f2a5be117ea9365324a3
Stephanie Weirich <sweirich@galois.com>
2018-12-06 13:42:05 -0500

    more missing files

---
+++ crux-mir/mir_verif/src/ops/index.rs
@@ -0,0 +1,6 @@
+#![feature(i128_type)]
+#![crate_type = "lib"]
+#![no_implicit_prelude]
+
+#![feature(slice_get_slice)]
+
---
+++ crux-mir/test/conc_eval/clos/fn_static_poly.rs
@@ -0,0 +1,20 @@
+
+
+fn call_with_one<T>(some_closure: &Fn(T) -> T, arg:T) -> T {
+
+    some_closure(arg)
+
+}
+
+fn f (y:i32) -> i32 {
+
+    call_with_one(&|x| x + y, 1)
+
+}
+
+const ARG :i32 = 2;
+
+#[cfg(with_main)]
+fn main() {
+    println!("{:?}", f(ARG))
+}
