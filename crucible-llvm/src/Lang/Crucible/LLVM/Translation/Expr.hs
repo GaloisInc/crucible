GaloisInc/crucible

commit 17fdc13cb4d213d24f13b4d45f4b92c39287ed6a

Rearrange slightly how null-checking and coercing bitvector values to booleans works.  This formulation prefers to use the `BVNonzero` expression, which provides better simplifiation in the backend, resulting in simpler terms.

crucible-llvm/src/Lang/Crucible/LLVM/Translation/Expr.hs
@@ -46,6 +46,7 @@ module Lang.Crucible.LLVM.Translation.Expr
   , liftConstant
 
   , callIsNull
+  , callIntToBool
   , callAlloca
   , callPushFrame
   , callPopFrame
@@ -436,15 +437,22 @@ callIsNull
    => NatRepr w
    -> Expr (LLVM arch) s (LLVMPointerType w)
    -> LLVMGenerator h s arch ret (Expr (LLVM arch) s BoolType)
-callIsNull w (BitvectorAsPointerExpr _ bv) =
+callIsNull w ex = App . Not <$> callIntToBool w ex
+
+callIntToBool
+  :: (1 <= w)
+  => NatRepr w
+  -> Expr (LLVM arch) s (LLVMPointerType w)
+  -> LLVMGenerator h s arch ret (Expr (LLVM arch) s BoolType)
+callIntToBool w (BitvectorAsPointerExpr _ bv) =
   case bv of
     App (BVLit _ 0) -> return true
-    _ -> return (App (BVEq w bv (App (BVLit w 0))))
-callIsNull w ex =
+    _ -> return (App (BVNonzero w bv))
+callIntToBool w ex =
    do ex' <- forceEvaluation (App (UnrollRecursive knownRepr (Ctx.Empty :> BVRepr w) ex))
       let blk = App (GetStruct ex' (Ctx.natIndex @0) NatRepr)
       let off = App (GetStruct ex' (Ctx.natIndex @1) (BVRepr w))
-      return (blk .== litExpr 0 .&& (App (BVEq w off (App (BVLit w 0)))))
+      return (blk ./= litExpr 0 .|| (App (BVNonzero w off)))
 
 callAlloca
    :: wptr ~ ArchWidth arch

crucible-llvm/src/Lang/Crucible/LLVM/Translation/Instruction.hs
@@ -1348,7 +1348,7 @@ generateInstr retType lab instr assign_f k =
          x' <- transTypedValue x
          y' <- transTypedValue (L.Typed (L.typedType x) y)
          e' <- case asScalar c' of
-                 Scalar (LLVMPointerRepr w) e -> notExpr <$> callIsNull w e
+                 Scalar (LLVMPointerRepr w) e -> callIntToBool w e
                  _ -> fail "expected boolean condition on select"
 
          ifte_ e' (assign_f x') (assign_f y')
@@ -1359,7 +1359,7 @@ generateInstr retType lab instr assign_f k =
     L.Br v l1 l2 -> do
         v' <- transTypedValue v
         e' <- case asScalar v' of
-                 Scalar (LLVMPointerRepr w) e -> notExpr <$> callIsNull w e
+                 Scalar (LLVMPointerRepr w) e -> callIntToBool w e
                  _ -> fail "expected boolean condition on branch"
 
         phi1 <- defineBlockLabel (definePhiBlock lab l1)

commit ada73f752f67bafff9ca9dae61471c1a48754997

Fix typo introduced in 17fdc13c.
callIntToBool should return false when the input is 0.

crucible-llvm/src/Lang/Crucible/LLVM/Translation/Expr.hs
@@ -446,7 +446,7 @@ callIntToBool
   -> LLVMGenerator h s arch ret (Expr (LLVM arch) s BoolType)
 callIntToBool w (BitvectorAsPointerExpr _ bv) =
   case bv of
-    App (BVLit _ 0) -> return true
+    App (BVLit _ 0) -> return false
     _ -> return (App (BVNonzero w bv))
 callIntToBool w ex =
    do ex' <- forceEvaluation (App (UnrollRecursive knownRepr (Ctx.Empty :> BVRepr w) ex))