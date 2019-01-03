bab8805bfd464f04bfec401fec97cee437584713
Rob Dockins <rdockins@galois.com>
2019-01-02 16:50:17 -0800

    Increase the verbosity required to see the 'signatures mismatch' message, which is not necessarily an error condition because of how we handle the situation where LLVM changes the types of intrinsics.

--- crucible-llvm/src/Lang/Crucible/LLVM/Intrinsics.hs
+++ crucible-llvm/src/Lang/Crucible/LLVM/Intrinsics.hs
@@ -374,7 +374,7 @@ register_llvm_override llvmOverride = do
   if (requestedDecl /= decl) then
     do when (L.decName requestedDecl == L.decName decl) $
          do logFn <- lift $ lift $ lift $ getLogFunction
-            liftIO $ logFn 1 $ unwords
+            liftIO $ logFn 3 $ unwords
               [ "Mismatch declaration signatures"
               , " *** requested: " ++ show requestedDecl
               , " *** found: "     ++ show decl
