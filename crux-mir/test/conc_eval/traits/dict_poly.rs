0acad090f46fe38485c0fd6aec6d432551623ff0
Stephanie Weirich <sweirich@galois.com>
2019-01-30 16:32:34 -0500

    test case for dictionary resolution

---
+++ crux-mir/test/conc_eval/traits/dict_poly.rs
@@ -0,0 +1,37 @@
+// a static trait invocation for a polymorphic type
+// calling the g method in h requires a dictionary argument 
+
+struct Data<T>(T);
+
+trait G {
+    fn g (&self) -> u32;
+}
+
+impl G for u32 {
+    fn g(&self) -> u32 {
+        return 1
+    }
+}
+
+impl <T:G> G for Data<T> {
+    fn g(&self) -> u32 {
+       return 2 // self.0.g()
+    }
+}
+
+
+fn h<T>(x:T) -> u32 where T:G {
+    x.g()
+}
+
+fn f(_: ()) -> u32 {
+    let d = Data(Data(32));
+    d.g()
+}
+
+const ARG: () = ();
+
+#[cfg(with_main)]
+fn main() {
+   println!("{:?}", f(ARG));
+}
