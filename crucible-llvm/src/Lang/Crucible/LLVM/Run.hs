dba7171158859a85708201c3213475674f4e81f7
Iavor Diatchki <iavor.diatchki@gmail.com>
2019-05-21 10:15:30 -0700

    Add a module to help run Crucible on LLVM modules.
    The `Setup` record is pretty generic, and can be reused by non-LLVM
    architectures, so perhaps it should go somewhere else.

---
+++ crucible-llvm/src/Lang/Crucible/LLVM/Run.hs
@@ -0,0 +1,116 @@
+{-# Language RankNTypes, ExistentialQuantification #-}
+{-# Language ImplicitParams #-}
+{-# Language RecordWildCards #-}
+
+{- | Utilities for running Crucible on a bit-code file. -}
+module Lang.Crucible.LLVM.Run
+  ( Crux(..), Setup(..)
+  , runCrux
+  , Module
+  ) where
+
+import Control.Lens((^.))
+import Control.Monad.ST(stToIO,RealWorld)
+import System.IO(Handle)
+
+
+import Data.Parameterized.Some(Some(..))
+import Data.Parameterized.Context(EmptyCtx)
+
+import Lang.Crucible.Types(TypeRepr)
+import Lang.Crucible.Simulator
+  ( RegEntry, RegValue
+  , fnBindingsFromList, runOverrideSim
+  , initSimContext
+  , ExecState( InitialState ), defaultAbortHandler
+  , OverrideSim
+  )
+import Lang.Crucible.FunctionHandle(HandleAllocator,newHandleAllocator)
+
+import Text.LLVM.AST (Module)
+
+import Lang.Crucible.LLVM.Intrinsics
+        (llvmIntrinsicTypes, llvmPtrWidth, llvmTypeCtx , LLVM)
+import Lang.Crucible.LLVM(llvmExtensionImpl, llvmGlobals)
+import Lang.Crucible.LLVM.Translation
+        (globalInitMap,transContext,translateModule,ModuleTranslation)
+import Lang.Crucible.LLVM.Globals(populateAllGlobals,initializeMemory)
+
+import Lang.Crucible.LLVM.MemModel(withPtrWidth)
+
+
+import Lang.Crucible.Backend(IsSymInterface)
+
+
+
+{-| Curcible initialization.  This is just a wrapper that ensure that
+the initialization code does not make assumptions about the LLVM architecture.
+-}
+newtype Crux res = Crux (forall arch. Setup (LLVM arch) res)
+
+-- | Generic Crucible initialization.
+data Setup arch res =
+  forall p t sym. IsSymInterface sym =>
+  Setup
+  { cruxOutput :: Handle
+    -- ^ Print stuff here.
+
+  , cruxBackend :: sym
+    -- ^ Use this backend.
+
+  , cruxInitState :: p
+    -- ^ Initial value for the user state.
+
+  , cruxInitCode ::
+      OverrideSim p sym arch (RegEntry sym t) EmptyCtx t (RegValue sym t)
+    -- ^ Initialization code to run at the start of simulation.
+
+  , cruxInitCodeReturns :: TypeRepr t
+    -- ^ Type of value returned by initialization code.
+
+  , cruxGo :: ExecState p sym arch (RegEntry sym t) -> IO res
+    -- ^ Do something with the simulator's initial state.
+
+  }
+
+-- | Run Crucible with the given initialization, on the given LLVM module.
+runCrux :: Crux res -> Module -> IO res
+runCrux (Crux setup) llvm_mod =
+  do halloc     <- newHandleAllocator
+     Some trans <- stToIO (translateModule halloc llvm_mod)
+     withTranslated halloc llvm_mod trans setup
+
+-- | This is split off from 'runCrux' for the type signutre, and also
+-- we only use the initialization bits in here.
+withTranslated ::
+  HandleAllocator RealWorld ->
+  Module ->
+  ModuleTranslation arch ->
+  Setup (LLVM arch) res ->
+  IO res
+withTranslated halloc llvm_mod trans Setup { .. } =
+  do let llvmCtxt = trans    ^. transContext
+     let ?lc      = llvmCtxt ^. llvmTypeCtx  -- used by 'populateAllGlobals'
+
+     llvmPtrWidth llvmCtxt $ \ptrW -> withPtrWidth ptrW $
+
+       do mem <- do mem0 <- initializeMemory cruxBackend llvmCtxt llvm_mod
+                    populateAllGlobals cruxBackend (globalInitMap trans) mem0
+
+          let globSt = llvmGlobals llvmCtxt mem
+          let simctx = initSimContext
+                          cruxBackend
+                          llvmIntrinsicTypes
+                          halloc
+                          cruxOutput
+                          (fnBindingsFromList [])
+                          llvmExtensionImpl
+                          cruxInitState
+
+          cruxGo $ InitialState simctx globSt defaultAbortHandler
+                 $ runOverrideSim cruxInitCodeReturns cruxInitCode
+
+
+
+
+
