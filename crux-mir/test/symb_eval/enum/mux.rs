a82dbff0c636f63bc0a017fcaf7766be5f377de6
Stuart Pernsteiner <spernsteiner@galois.com>
2019-09-11 09:30:15 -0700

    add failing test for muxing of enums containing structs

---
+++ crux-mir/test/symb_eval/enum/mux.rs
@@ -0,0 +1,18 @@
+// FAIL: cannot mux ANYs of different underlying type
+#![feature(custom_attribute)]
+extern crate crucible;
+
+struct S {
+    val: u8,
+}
+
+fn f(x: bool) -> Option<S> {
+    if x { Some(S { val: 1 }) } else { None }
+}
+
+#[crux_test]
+fn test() {
+    let x = crucible::crucible_u8("x") != 0;
+    let y = f(x);
+    if x { y.unwrap(); }
+}
