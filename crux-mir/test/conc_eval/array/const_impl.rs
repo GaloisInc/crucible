4dffc2f36767f30d7a3cff43f655af2d4cb515ae
Stuart Pernsteiner <spernsteiner@galois.com>
2019-09-12 10:15:47 -0700

    add array/const_impl test

---
+++ crux-mir/test/conc_eval/array/const_impl.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(not(with_main), no_std)]
+#![cfg_attr(not(with_main), feature(custom_attribute))]
+// Tests calls to methods defined in const impls.
+
+pub fn f(x: i32) -> usize {
+    let arr: [i32; 5] = [x; 5];
+    arr.as_ref().len()
+}
+
+pub static ARG: i32 = 1;
+
+#[cfg(with_main)] pub fn main() { println!("{:?}", f(ARG)); }
+#[cfg(not(with_main))] #[crux_test] fn crux_test() -> usize { f(ARG) }
