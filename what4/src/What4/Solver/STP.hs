f48ffabf6497d1936468f1e881319e4f38f2379f
Rob Dockins <rdockins@galois.com>
2019-10-09 14:32:03 -0700

    remove the unneeded acknolwedgement action argument from `writeDefaultSMT2`

--- what4/src/What4/Solver/STP.hs
+++ what4/src/What4/Solver/STP.hs
@@ -66,7 +66,7 @@ stpAdapter =
   , solver_adapter_config_options = stpOptions
   , solver_adapter_check_sat  = runSTPInOverride
   , solver_adapter_write_smt2 =
-       SMT2.writeDefaultSMT2 STP nullAcknowledgementAction "STP" defaultWriteSMTLIB2Features
+       SMT2.writeDefaultSMT2 STP "STP" defaultWriteSMTLIB2Features
   }
 
 instance SMT2.SMTLib2Tweaks STP where
