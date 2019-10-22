28d3f4bd1b766aaccfc6251739f6e87552847e4f
Stuart Pernsteiner <spernsteiner@galois.com>
2019-10-22 11:22:32 -0700

    fix some build errors in tests

--- crux-mir/test/conc_eval/refs/imm_raw.rs
+++ crux-mir/test/conc_eval/refs/imm_raw.rs
@@ -14,4 +14,4 @@ pub fn f(x: i32) -> i32 {
 pub static ARG: i32 = 1;
 
 #[cfg(with_main)] pub fn main() { println!("{:?}", f(ARG)); }
-#[cfg(not(with_main))] #[crux_test] fn crux_test() ->  { f(ARG) }
+#[cfg(not(with_main))] #[crux_test] fn crux_test() -> i32 { f(ARG) }
