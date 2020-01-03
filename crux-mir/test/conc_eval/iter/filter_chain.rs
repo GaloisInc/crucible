6954a3964fde7852706b7507a7f87a29929db4f3
Stuart Pernsteiner <spernsteiner@galois.com>
2020-01-03 15:07:08 -0800

    add filter_chain test for type nesting performance

---
+++ crux-mir/test/conc_eval/iter/filter_chain.rs
@@ -0,0 +1,28 @@
+#![cfg_attr(not(with_main), feature(custom_attribute))]
+
+fn f(x: u8) -> u8 {
+    let opt = Some((x, x));
+    // This sort of chaining used to cause exponential growth in the size of the .mir file, with
+    // the translation timing out around 5 levels of chaining.
+    opt.into_iter()
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .filter(|&(a, b)| a == b)
+        .for_each(|(a, b)| {});
+    0
+}
+
+const ARG: u8 = 42;
+
+#[cfg(with_main)]
+pub fn main() {
+    println!("{:?}", f(ARG));
+}
+#[cfg(not(with_main))] #[crux_test] fn crux_test() -> u8 { f(ARG) }
