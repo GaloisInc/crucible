979fd1f683c770c6244b578da1dd53fe63bfc61d
Stuart Pernsteiner <spernsteiner@galois.com>
2020-01-17 17:04:51 -0800

    add test cases that rely on adt monomorphization

---
+++ crux-mir/test/conc_eval/enum/cow.rs
@@ -0,0 +1,13 @@
+#![feature(custom_attribute)]
+use std::borrow::Cow;
+
+#[crux_test]
+fn crux_test() -> u8 {
+    let x: Cow<[u8]> = Cow::Borrowed(b"foo");
+    let y: Cow<[u8]> = Cow::Owned(b"bar".to_vec());
+    x[0] + y[0]
+}
+
+pub fn main() {
+    println!("{:?}", crux_test());
+}
