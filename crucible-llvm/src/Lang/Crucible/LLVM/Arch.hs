e65131d7bf1d10deb28953362f9ad98d044f354b
Rob Dockins <rdockins@galois.com>
2020-02-05 14:47:39 -0800

    Remove the use of the `WithAssertions` expression former in crucible-llvm. Instead, a language-extension expression former `LLVM_SideConditions` has been added that directly references the `LLVM_SafetyCondition` type.

--- crucible-llvm/src/Lang/Crucible/LLVM/Arch.hs
+++ crucible-llvm/src/Lang/Crucible/LLVM/Arch.hs
@@ -1,20 +1,30 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 module Lang.Crucible.LLVM.Arch
   ( llvmExtensionEval
   ) where
+
+import           Control.Lens ( (^.), to )
+import           Control.Monad (forM_)
+import qualified Data.List.NonEmpty as NE
+import           Data.Parameterized.TraversableF
 
 import           What4.Interface
 
 import           Lang.Crucible.Backend
 import           Lang.Crucible.Simulator.Intrinsics
 import           Lang.Crucible.Simulator.Evaluation
+import           Lang.Crucible.Simulator.RegValue
+import           Lang.Crucible.Simulator.SimError
 
 import qualified Lang.Crucible.LLVM.Arch.X86 as X86
 import           Lang.Crucible.LLVM.Extension
+import           Lang.Crucible.LLVM.Extension.Safety
 import           Lang.Crucible.LLVM.MemModel.Pointer
 
-llvmExtensionEval ::
+llvmExtensionEval :: forall sym arch.
   IsSymInterface sym =>
   sym ->
   IntrinsicTypes sym ->
@@ -24,6 +34,14 @@ llvmExtensionEval ::
 llvmExtensionEval sym _iTypes _logFn eval e =
   case e of
     X86Expr ex -> X86.eval sym eval ex
+
+    LLVM_SideConditions _tp conds val ->
+      do conds' <- traverse (traverseF (\x -> RV @sym <$> eval x)) (NE.toList conds)
+         forM_ conds' $ \c ->
+           do let bb = c^.classifier
+              let err = AssertFailureSimError (show (explainBB bb)) (show (detailBB bb))
+              assert sym (c^.predicate.to unRV) err
+         eval val
 
     LLVM_PointerExpr _w blk off ->
       do blk' <- eval blk
