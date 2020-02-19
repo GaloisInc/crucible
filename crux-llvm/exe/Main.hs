dc57e6f12e68473320adca17661f1cf6385ec81d
Rob Dockins <rdockins@galois.com>
2020-02-18 16:52:48 -0800

    For crux-llvm on Posix systems, intercept SIGTERM and convert it to SIGINT for the whole process group, which mimics the user pressing CTRL-C, and performs better process cleanup.

--- crux-llvm/exe/Main.hs
+++ crux-llvm/exe/Main.hs
@@ -1,3 +1,23 @@
 module Main (main) where
 
-import CruxLLVMMain (main)
+import Control.Monad
+import System.Exit
+import System.Posix.Signals
+import System.Posix.Process
+
+import CruxLLVMMain (mainWithOutputConfig, defaultOutputConfig)
+
+-- Convert a SIGTERM signal into a SIGINT signal for the entire process group
+installSIGTERMHandler :: IO ()
+installSIGTERMHandler =
+  do gid <- getProcessGroupID
+     void $ installHandler sigTERM (CatchOnce (handler gid)) Nothing
+ where
+ handler gid = signalProcessGroup sigINT gid
+
+
+main :: IO ()
+main =
+  do installSIGTERMHandler
+     ec <- mainWithOutputConfig defaultOutputConfig
+     exitWith ec
