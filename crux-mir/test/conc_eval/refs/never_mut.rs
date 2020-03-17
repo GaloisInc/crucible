552f513fbf337997ad1543c166efd6f40e407875
Stuart Pernsteiner <spernsteiner@galois.com>
2020-03-17 14:15:17 -0700

    add tests for mutable reference initialization

---
+++ crux-mir/test/conc_eval/refs/never_mut.rs
@@ -0,0 +1,18 @@
+#![feature(custom_attribute)]
+#![feature(never_type)]
+
+#[crux_test]
+fn crux_test() -> i32 {
+    let x: Result<i32, !> = Ok(1);
+    match x {
+        Ok(x) => x,
+        Err(mut e) => {
+            let r = &mut e;
+            panic!();
+        },
+    }
+}
+
+pub fn main() {
+    println!("{:?}", crux_test());
+}
