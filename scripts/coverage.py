a4ab8f1fcf8af8c53a6b3bc404c1fe3a204b0055
Stuart Pernsteiner <spernsteiner@galois.com>
2020-05-05 09:45:52 -0700

    support multiple input files in coverage.py

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -7,22 +7,24 @@ def read_report_data(f):
     end = buf.rindex(b')')
     return json.loads(buf[start + 1 : end])
 
-j = read_report_data(sys.stdin.buffer)
-
 branches = {}
 visited = set()
 
-for x in j:
-    if x['type'] != 'callgraph':
-        continue
-    for evt in x['events']:
-        if evt['type'] == 'BRANCH':
-            k = (evt['function'], evt['callsite'], tuple(evt['blocks']))
-            branches[k] = evt
-        elif evt['type'] == 'BLOCK':
-            func = evt['function']
-            for b in evt['blocks']:
-                visited.add((func, b))
+for fn in sys.argv[1:]:
+    with open(fn, 'rb') as f:
+        j = read_report_data(f)
+
+    for x in j:
+        if x['type'] != 'callgraph':
+            continue
+        for evt in x['events']:
+            if evt['type'] == 'BRANCH':
+                k = (evt['function'], evt['callsite'], tuple(evt['blocks']))
+                branches[k] = evt
+            elif evt['type'] == 'BLOCK':
+                func = evt['function']
+                for b in evt['blocks']:
+                    visited.add((func, b))
 
 for evt in branches.values():
     func = evt['function']
