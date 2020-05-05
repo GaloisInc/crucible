9c883e11f57c3f6c0483488c25bf29a73789af6d
Stuart Pernsteiner <spernsteiner@galois.com>
2020-05-05 10:48:02 -0700

    coverage.py: merge branches with the same source location

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -1,3 +1,4 @@
+from collections import namedtuple, defaultdict
 import json
 import sys
 
@@ -7,31 +8,54 @@ def read_report_data(f):
     end = buf.rindex(b')')
     return json.loads(buf[start + 1 : end])
 
-branches = {}
-visited = set()
+assert len(sys.argv) > 1, 'expected at least one input file'
+js = [read_report_data(open(fn, 'rb')) for fn in sys.argv[1:]]
 
-for fn in sys.argv[1:]:
-    with open(fn, 'rb') as f:
-        j = read_report_data(f)
-
+# Collect all events
+evts = []
+for j in js:
     for x in j:
         if x['type'] != 'callgraph':
             continue
-        for evt in x['events']:
-            if evt['type'] == 'BRANCH':
-                k = (evt['function'], evt['callsite'], tuple(evt['blocks']))
-                branches[k] = evt
-            elif evt['type'] == 'BLOCK':
-                func = evt['function']
-                for b in evt['blocks']:
-                    visited.add((func, b))
+        evts.extend(x['events'])
 
-for evt in branches.values():
-    func = evt['function']
-    missing = [i for i,b in enumerate(evt['blocks']) if (func, b) not in visited]
+# Build tables of branches and visited blocks
+branches = set()
+visited = set()
+for evt in evts:
+    if evt['type'] == 'BRANCH':
+        branches.add((evt['function'], evt['callsite'], tuple(evt['blocks'])))
+    elif evt['type'] == 'BLOCK':
+        func = evt['function']
+        for b in evt['blocks']:
+            visited.add((func, b))
+
+BranchInfo = namedtuple('BranchInfo', ('function', 'callsite', 'blocks', 'taken'))
+
+branch_info = []
+for (function, callsite, blocks) in branches:
+    taken = [(function, b) in visited for b in blocks]
+    branch_info.append(BranchInfo(function, callsite, blocks, taken))
+
+# Merge branches by callsite.  We want to treat instances of the same source
+# branch in different monomorphizations as a single branch for coverage
+# purposes.
+by_callsite = defaultdict(list)
+for bi in branch_info:
+    by_callsite[bi.callsite].append(bi)
+
+for callsite in sorted(by_callsite.keys()):
+    bis = by_callsite[callsite]
+    if len(set(bi.function for bi in bis)) != len(bis):
+        # Multiple distinct branches within a single function.  We can't
+        # distinguish them, so the results may overestimate coverage.
+        print('warning: found multiple branches at %s' % callsite)
+    taken = [False] * max(len(bi.taken) for bi in bis)
+    for bi in bis:
+        for i, t in enumerate(bi.taken):
+            if t:
+                taken[i] = True
+    missing = [i for i,t in enumerate(taken) if not t]
     if len(missing) == 0:
         continue
-    print('exit %s not taken at %s' % (missing, evt['callsite']))
-
-
-
+    print('exit %s not taken at %s' % (missing, callsite))
