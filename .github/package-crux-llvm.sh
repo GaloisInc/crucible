24dbe953858f9f979e85405e60cee06cf3819ab1
Aaron Tomb <atomb@galois.com>
2020-09-15 14:04:10 -0700

    Conditionally build crux-llvm-svcomp
    Omit Windows

--- .github/package-crux-llvm.sh
+++ .github/package-crux-llvm.sh
@@ -3,14 +3,22 @@ set -Eeuo pipefail
 
 DATE=`date "+%Y-%m-%d"`
 PKG=crux-llvm-$DATE
+EXE=`cabal v2-exec which crux-llvm`
+EXE_SVCOMP=`cabal v2-exec which crux-llvm-svcomp`
+
 rm -rf $PKG
-cabal v2-build exe:crux-llvm exe:crux-llvm-svcomp
+
 mkdir $PKG
 mkdir $PKG/bin
 mkdir $PKG/doc
-cp `cabal v2-exec which crux-llvm` $PKG/bin
-cp `cabal v2-exec which crux-llvm-svcomp` $PKG/bin
+
+cp $EXE $PKG/bin
+if [ -e $EXE_SVCOMP ] ; then
+  cp $EXE_SVCOMP $PKG/bin
+fi
 cp crux-llvm/README.md $PKG/doc
 cp -r crux-llvm/c-src $PKG
+
 tar cf $PKG.tar.gz $PKG
+
 rm -rf $PKG
