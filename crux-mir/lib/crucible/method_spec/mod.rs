a20bb7e4e9bcccc7ca2fab48e3aa57b46248c051
Stuart Pernsteiner <spernsteiner@galois.com>
2020-11-04 14:52:31 -0800

    libcrucible: add bindings for future MethodSpec and MethodSpecBuilder types

---
+++ crux-mir/lib/crucible/method_spec/mod.rs
@@ -0,0 +1,67 @@
+//! Provides the `MethodSpec` type, used for compositional reasoning.  Also provides
+//! `MethodSpecBuilder`, which is used internally by the compositional reasoning macros to
+//! construct a `MethodSpec` from a symbolic test.
+use core::fmt;
+
+mod raw;
+
+
+/// The specification of a function.  This can be used when verifying callers of the function to
+/// avoid simulating the entire function itself.
+#[derive(Clone, Copy)]
+pub struct MethodSpec {
+    raw: raw::MethodSpec,
+}
+
+impl fmt::Debug for MethodSpec {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        let s = raw::spec_pretty_print(self.raw);
+        fmt.write_str("MethodSpec {")?;
+        for (i, chunk) in s.split("\n").enumerate() {
+            if i > 0 {
+                fmt.write_str(", ")?;
+            }
+            fmt.write_str(chunk);
+        }
+        fmt.write_str("}")?;
+        Ok(())
+    }
+}
+
+
+/// Helper type used to incrementally construct a `MethodSpec` for a 
+pub struct MethodSpecBuilder {
+    raw: raw::MethodSpecBuilder,
+}
+
+impl fmt::Debug for MethodSpecBuilder {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "MethodSpecBuilder {{ .. }}")
+    }
+}
+
+impl MethodSpecBuilder {
+    pub fn new<Args, F: Fn<Args>>(f: F) -> MethodSpecBuilder {
+        MethodSpecBuilder {
+            raw: raw::builder_new::<Args, F::Output>(),
+        }
+    }
+
+    pub fn add_arg<T>(&mut self, x: &T) {
+        self.raw = raw::builder_add_arg(self.raw, x);
+    }
+
+    pub fn set_return<T>(&mut self, x: &T) {
+        self.raw = raw::builder_set_return(self.raw, x);
+    }
+
+    pub fn gather_asserts(&mut self) {
+        self.raw = raw::builder_gather_asserts(self.raw);
+    }
+
+    pub fn finish(self) -> MethodSpec {
+        MethodSpec {
+            raw: raw::builder_finish(self.raw),
+        }
+    }
+}
