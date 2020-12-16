1dcf4de42e5ab5faf82a62f80210b1c75a718ca5
Brian Huffman <huffman@galois.com>
2020-12-16 02:28:59 -0800

    Refactor function `resolveField` into two parts.

--- crucible-jvm/src/Lang/Crucible/JVM/Translation/Class.hs
+++ crucible-jvm/src/Lang/Crucible/JVM/Translation/Class.hs
@@ -813,13 +813,12 @@ newInstanceInstr cls fieldIds = do
 -- the JVM context to determine the class where the field was actually
 -- declared (which may be a superclass of the one specified in the
 -- input 'J.FieldId'), as specified in section 5.4.3.2 "Field
--- Resolution" of the Java Virtual Machine Specification. Then return
--- a key that can be used with an object's field map.
-resolveField :: J.FieldId -> JVMGenerator s ret (Expr JVM s (StringType Unicode))
+-- Resolution" of the Java Virtual Machine Specification.
+resolveField :: J.FieldId -> JVMGenerator s ret J.FieldId
 resolveField fieldId =
   do cls <- lookupClassGen (J.fieldIdClass fieldId)
      case any fieldMatch (J.classFields cls) of
-       True -> pure (App (StringLit (UnicodeLiteral (fieldIdText fieldId))))
+       True -> pure fieldId
        False ->
          -- otherwise recursively check the superclass
          case J.superClass cls of
@@ -830,13 +829,17 @@ resolveField fieldId =
     fieldMatch f =
       (J.fieldName f, J.fieldType f) == (J.fieldIdName fieldId, J.fieldIdType fieldId)
 
+-- | Convert a 'J.FieldId' into a string key that can be used with an object's field map.
+fieldIdKey :: J.FieldId -> Expr JVM s (StringType Unicode)
+fieldIdKey fieldId = App (StringLit (UnicodeLiteral (fieldIdText fieldId)))
+
 -- | Access the field component of a JVM object (must be a class instance, not an array).
 getInstanceFieldValue :: JVMObject s -> J.FieldId -> JVMGenerator s ret (JVMValue s)
 getInstanceFieldValue obj fieldId =
   do let uobj = App (UnrollRecursive knownRepr knownRepr obj)
      inst <- projectVariant "getfield: expected class instance" Ctx.i1of2 uobj
      let fields = App (GetStruct inst Ctx.i1of2 knownRepr)
-     key <- resolveField fieldId
+     key <- fieldIdKey <$> resolveField fieldId
      let mval = App (LookupStringMapEntry knownRepr fields key)
      dyn <- assertedJustExpr mval "Field not present"
      fromJVMDynamic ("getfield " <> fieldIdText fieldId) (J.fieldIdType fieldId) dyn
@@ -849,7 +852,7 @@ setInstanceFieldValue obj fieldId val =
      let uobj = App (UnrollRecursive knownRepr knownRepr obj)
      inst <- projectVariant "setfield: expected class instance" Ctx.i1of2 uobj
      let fields = App (GetStruct inst Ctx.i1of2 knownRepr)
-     key <- resolveField fieldId
+     key <- fieldIdKey <$> resolveField fieldId
      let fields' = App (InsertStringMapEntry knownRepr fields key mdyn)
      let inst'  = App (SetStruct knownRepr inst Ctx.i1of2 fields')
      let uobj' = App (InjectVariant knownRepr Ctx.i1of2 inst')
