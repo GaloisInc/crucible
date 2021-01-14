410858a2778022179182edafa0bd877624fd6f7c
Kevin Quick <kquick@galois.com>
2021-01-14 10:23:15 -0800

    [crucible-llvm] Do compile/assemble/translate as individual tests
    Various cleanup in tests.
    Enables the global extern test.
    Builds outputs in the test directory instead of the top level.

--- crucible-llvm/test/Tests.hs
+++ crucible-llvm/test/Tests.hs
@@ -1,13 +1,8 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE BangPatterns     #-}
 {-# LANGUAGE DataKinds        #-}
-{-# LANGUAGE ExplicitForAll   #-}
-{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ImplicitParams   #-}
 {-# LANGUAGE LambdaCase       #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Main where
 
@@ -30,12 +25,14 @@ import qualified Test.Tasty.Options as TO
 import qualified Test.Tasty.Runners as TR
 
 -- General
-import           Data.Foldable
+import           Data.Either ( fromRight )
+import           Data.Maybe ( catMaybes )
+import           System.FilePath ( (-<.>), splitExtension, splitFileName )
 import           Data.Proxy ( Proxy(..) )
 import           Control.Monad
 import qualified Data.Map.Strict as Map
 import qualified System.Directory as Dir
-import           System.Exit (exitFailure, ExitCode(..))
+import           System.Exit ( ExitCode(..) )
 import qualified System.Process as Proc
 
 -- Modules being tested
@@ -73,6 +70,14 @@ doProc !exe !args = do
   where exitCodeToInt ExitSuccess     = 0
         exitCodeToInt (ExitFailure i) = i
 
+assertProcSuccess :: String -> String -> (Int, String, String) -> Assertion
+assertProcSuccess msg file (exitCode, stdout, stderr) = do
+  when (exitCode /= 0) $ do
+    putStrLn $ msg ++ " " ++ file ++ " failure"
+    putStrLn stdout
+    putStrLn stderr
+  exitCode == 0 @? msg ++ " " ++ file ++ " attempt failed with " ++ show exitCode
+
 
 -- | Compile a C file with clang, returning the exit code
 compile :: String -> FilePath -> IO (Int, String, String)
@@ -99,99 +104,103 @@ llvmTestIngredients = includingOptions [ TO.Option (Proxy @LLVMAssembler)
                                        ] : defaultIngredients
 
 main :: IO ()
-main = do
-  -- Parse the command line options using Tasty's default settings; the normal
-  -- 'askOption' combinators only work inside of the 'TestTree', which doesn't
-  -- help for this setup code. We have to pass in some TestTree, but it doesn't
-  -- really matter for this use case
-  let emptyTestGroup = testGroup "emptyTestGroup" []
-  opts <- TR.parseOptions llvmTestIngredients emptyTestGroup
-  let LLVMAssembler llvm_as = TO.lookupOption @LLVMAssembler opts
-  let Clang clang = TO.lookupOption @Clang opts
+main = defaultMainWithIngredients llvmTestIngredients $
+  testGroup "Tests"
+    [ functionTests
+    , globalTests
+    , memoryTests
+    , translationTests
 
-  wd <- Dir.getCurrentDirectory
-  putStrLn $ "Looking for tests in " ++ wd
+    , testBuildTranslation "test/c/global-int.c" $ \getTrans ->
+        testCase "valid global integer symbol reference" $ do
+        Some t <- getTrans
+        Map.singleton (L.Symbol "x") (Right $ (i32, Just $ IntConst (knownNat @32) (BV.mkBV knownNat 42))) @=?
+          Map.map snd (globalInitMap t)
 
-  let prepend pr = map (\s -> pr ++ s)
-  let cfiles     = prepend "global" [ "-int", "-struct", "-uninitialized", "-extern" ]
-  let llfiles    = ["lifetime"]
-  let append ext = map (\s -> s ++ ext)
-  let assertSuccess msg file (exitCode, stdout, stderr) = do
-        when (exitCode /= 0) $ do
-          putStrLn $ msg ++ " " ++ file
-          putStrLn stdout
-          putStrLn stderr
-          exitFailure
+    , testBuildTranslation "test/c/global-struct.c" $ \getTrans ->
+        testCase "valid global struct field symbol reference" $ do
+        Some t <- getTrans
+        IntConst (knownNat @32) (BV.mkBV knownNat 17) @=?
+          case snd <$> Map.lookup (L.Symbol "z") (globalInitMap t) of
+            Just (Right (_, Just (StructConst _ (x : _)))) -> x
+            _ -> IntConst (knownNat @1) (BV.zero knownNat)
 
-  putStrLn ("Compiling C code to LLVM bitcode with " ++ clang)
-  forM_ (prepend "test/c/" $ append ".c" cfiles) $ \file -> do
-    assertSuccess "compile" file =<< compile clang file
+    , testBuildTranslation "test/c/global-uninitialized.c" $ \getTrans ->
+        testCase "valid global unitialized variable reference" $ do
+        Some t <- getTrans
+        Map.singleton (L.Symbol "x") (Right $ (i32, Just $ ZeroConst i32)) @=?
+          Map.map snd (globalInitMap t)
 
-  putStrLn ("Assembling LLVM assembly with " ++ llvm_as)
-  forM_ (zip (prepend "test/ll/" $ append ".ll" llfiles)
-             (append ".bc" llfiles)) $ \(inputFile, outputFile) -> do
-    assertSuccess "assemble" inputFile =<< assemble llvm_as inputFile outputFile
+    , testBuildTranslation "test/c/global-extern.c" $ \getTrans ->
+        testCase "valid global extern variable reference" $ do
+        Some t <- getTrans
+        Map.singleton (L.Symbol "extern_int") (Right (i32, Nothing)) @=?
+          Map.map snd (globalInitMap t)
 
-  putStrLn "Parsing LLVM bitcode"
-  -- parsed :: [Module]
-  parsed <-
-    forM (append ".bc" (cfiles ++ llfiles)) $ \file -> do
-    parsed <- parseLLVM file
-    case parsed of
-      Left err -> do
-        putStrLn $ "Failed to parse " ++ file
-        putStrLn err
-        exitFailure
-      Right m  -> pure m
+    , testBuildTranslation "test/ll/lifetime.ll" $ \_getTrans ->
+        -- We're really just checking that the translation succeeds without
+        -- exceptions.
+        testCase "lifetime test" $ return ()
 
-  putStrLn "Translating LLVM modules"
-  halloc     <- newHandleAllocator
-  -- translated :: [ModuleTranslation]
-  let ?laxArith = False
-  translated <- traverse (translateModule halloc) parsed
-
-  -- Run tests on the results
-  case translated of
-    [Some g1, Some g2, Some g3, Some g4, Some l1] ->
-      defaultMainWithIngredients llvmTestIngredients (tests g1 g2 g3 g4 l1)
-    _ -> error "translation failure!"
-
-tests :: ModuleTranslation arch1
-      -> ModuleTranslation arch2
-      -> ModuleTranslation arch3
-      -> ModuleTranslation arch4
-      -> ModuleTranslation arch5
-      -> TestTree
-tests int struct uninitialized _ lifetime = do
-  testGroup "Tests" $ concat $
-    [[ functionTests
-     , globalTests
-     , memoryTests
-     , translationTests
-
-     ]] <>
-    [ [ testCase "int" $
-          Map.singleton (L.Symbol "x") (Right $ (i32, Just $ IntConst (knownNat @32) (BV.mkBV knownNat 42))) @=?
-             Map.map snd (globalInitMap int)
-      , testCase "struct" $
-          IntConst (knownNat @32) (BV.mkBV knownNat 17) @=?
-             case snd <$> Map.lookup (L.Symbol "z") (globalInitMap struct) of
-               Just (Right (_, Just (StructConst _ (x : _)))) -> x
-               _ -> IntConst (knownNat @1) (BV.zero knownNat)
-      , testCase "unitialized" $
-          Map.singleton (L.Symbol "x") (Right $ (i32, Just $ ZeroConst i32)) @=?
-             Map.map snd (globalInitMap uninitialized)
-      -- The actual value for this one contains the error message, so it's a pain
-      -- to type out. Uncomment this test to take a look.
-      -- , testCase "extern" $
-      --     Map.singleton (L.Symbol "x") (Left $ "") @=?
-      --        (globalMap extern)
-
-      -- We're really just checking that the translation succeeds without
-      -- exceptions.
-      , testCase "lifetime" $
-          False @=? Map.null (cfgMap lifetime)
-      ]
+    ]
 
 
+testBuildTranslation :: FilePath -> (IO (Some ModuleTranslation) -> TestTree) -> TestTree
+testBuildTranslation srcPath llvmTransTests =
+  -- n.b. srcPath may be a relative path
+  let (dName, srcName) = splitFileName srcPath
+      (fName, ext) = splitExtension srcName
+      bcPath = srcPath -<.> ".bc"
+      (_, bcName) = splitFileName bcPath
+
+      genBCName = case ext of
+                    ".c" -> "compile " <> fName
+                    ".ll" -> "assemble " <> fName
+                    _ -> error $ "Cannot build LLVM bitcode file from a " ++ ext ++ " file"
+      parseBCName = "parse " ++ fName ++ " bitcode"
+      translateName = "translate " ++ fName
+
+      c_compile =
+        if (ext == ".c")
+        then
+          Just $ askOption $ \(Clang clang) ->
+          testCase genBCName $
+          assertProcSuccess "compile" srcPath =<<
+              Dir.withCurrentDirectory dName (compile clang srcName)
+        else Nothing
+
+      llvm_assemble =
+        if (ext == ".ll")
+        then Just $ askOption $ \(LLVMAssembler llvm_as) ->
+          testCase genBCName $
+          assertProcSuccess "assemble" srcPath =<<
+            Dir.withCurrentDirectory dName (assemble llvm_as srcName bcName)
+        else Nothing
+
+      parse_bitcode =
+        testCase parseBCName $
+        parseLLVM bcPath >>= \case
+          Left err -> do
+            putStrLn $ "Failed to parse " ++ bcPath
+            putStrLn err
+            err @?= ""
+          Right _ -> pure ()
+
+      trans = do halloc <- newHandleAllocator
+                 let ?laxArith = False
+                 translateModule halloc =<<
+                   (fromRight (error "parsing was already verified") <$> parseLLVM bcPath)
+
+      translate_bitcode =
+        testCase translateName $ do
+        trans >>= \(Some modTrans) ->
+          not (Map.null $ cfgMap modTrans) @? "Translation of " ++ bcPath ++ " was empty (failed?)"
+
+
+  in testGroup srcPath $ catMaybes
+    [ c_compile
+    , llvm_assemble
+    , Just $ after AllSucceed genBCName     parse_bitcode
+    , Just $ after AllSucceed parseBCName   translate_bitcode
+    , Just $ after AllSucceed translateName (llvmTransTests trans)
     ]
