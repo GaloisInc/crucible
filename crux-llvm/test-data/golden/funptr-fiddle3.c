bed3c68be5965b053a1c517f28d399e916bdc0f3
Kevin Quick <kquick@galois.com>
2021-01-18 17:44:56 -0800

    [crux-llvm] add more tests modifying function pointers.

---
+++ crux-llvm/test-data/golden/funptr-fiddle3.c
@@ -0,0 +1,22 @@
+#include <crucible.h>
+
+int f(void) {
+    return 42;
+}
+
+unsigned long int g(void) {
+    return 99;
+}
+
+unsigned long int sel(int a) {
+    if (a < 10)
+        return (unsigned long int)(&f) + 0;
+    return (unsigned long int)(&g) + 0;
+}
+
+int main() {
+    int arg = 3;
+    int (*fptr)(void) = sel(arg);
+    check ((fptr)() == 42);
+    return 0;
+}
