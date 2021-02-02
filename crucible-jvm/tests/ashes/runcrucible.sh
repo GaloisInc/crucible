bd2ed44165ee4fedcf41ed53cb2d2fbae79796af
Ryan Scott <rscott@galois.com>
2021-02-02 13:37:01 -0500

    crucible-jvm: Add --java-bin-dirs
    This mirrors a similar recent change introduced to SAW in
    GaloisInc/saw-script#1030.
    
    While I was in town, I updated the two `crucible-jvm`–related test suites
    to use the `PATH` instead of the `-j` flag. I can't exactly confirm via CI
    if these changes work (see #634), but they appear to do the right thing
    locally.
    
    Fixes #633.

--- crucible-jvm/tests/ashes/runcrucible.sh
+++ crucible-jvm/tests/ashes/runcrucible.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-RTJAR=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home/jre/lib/rt.jar
-
-cabal new-exec crucible-jvm -- -j $RTJAR $@
+cabal new-exec crucible-jvm -- $@
--- crucible-jvm/tests/test.sh
+++ crucible-jvm/tests/test.sh
@@ -1,8 +1,5 @@
 TESTCASE=Str
 
-RTJAR=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home/jre/lib/rt.jar
-
-
 make $TESTCASE.class
 javap -c $TESTCASE
-cabal new-exec crucible-jvm -- -d 3 -j $RTJAR $TESTCASE.java
+cabal new-exec crucible-jvm -- -d 3 $TESTCASE.java
