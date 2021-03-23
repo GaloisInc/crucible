3cdf02c3c29fe8a438e5acdd7dea612fd34c44bf
Langston Barrett <langston.barrett@gmail.com>
2021-03-23 10:55:04 -0700

    uc-crux-llvm: Clean up documentation of CLI opts and configuration
    Also list units on explore budget

--- uc-crux-llvm/src/UCCrux/LLVM/Config.hs
+++ uc-crux-llvm/src/UCCrux/LLVM/Config.hs
@@ -20,6 +20,8 @@ import           Control.Applicative ((<|>))
 import           Control.Monad (when)
 import           Data.Functor ((<&>))
 import           Data.Word (Word64)
+import           Data.Text (Text)
+import qualified Data.Text as Text
 import           System.Exit (die)
 
 import qualified Crux.Config as Crux
@@ -68,6 +70,30 @@ processUCCruxLLVMOptions (initCOpts, initUCOpts) =
         )
     pure (appCtx, finalCOpts, initUCOpts {ucLLVMOptions = finalLLOpts})
 
+exploreDoc :: Text
+exploreDoc = "Run in exploration mode"
+
+reExploreDoc :: Text
+reExploreDoc = "Re-explore functions that have already been explored (i.e., have logs)"
+
+exploreBudgetDoc :: Text
+exploreBudgetDoc = "Budget for exploration mode (number of functions)"
+
+exploreTimeoutDoc :: Text
+exploreTimeoutDoc = "Hard timeout for exploration of a single function (seconds)"
+
+exploreParallelDoc :: Text
+exploreParallelDoc = "Explore different functions in parallel"
+
+entryPointsDoc :: Text
+entryPointsDoc = "Comma-separated list of functions to examine."
+
+skipDoc :: Text
+skipDoc = "List of functions to skip during exploration"
+
+verbDoc :: Text
+verbDoc = "Verbosity of logging. (0: minimal, 1: informational, 2: debug)"
+
 ucCruxLLVMConfig :: IO (Crux.Config UCCruxLLVMOptions)
 ucCruxLLVMConfig = do
   llvmOpts <- llvmCruxConfig
@@ -76,46 +102,14 @@ ucCruxLLVMConfig = do
       { Crux.cfgFile =
           UCCruxLLVMOptions
             <$> Crux.cfgFile llvmOpts
-            <*> Crux.section
-              "explore"
-              Crux.yesOrNoSpec
-              False
-              "Run in exploration mode"
-            <*> Crux.section
-              "re-explore"
-              Crux.yesOrNoSpec
-              False
-              "Re-explore functions that have already been explored (i.e., have logs)"
-            <*> Crux.section
-              "explore-budget"
-              Crux.numSpec
-              8
-              "Budget for exploration mode"
-            <*> Crux.section
-              "explore-timeout"
-              Crux.numSpec
-              5
-              "Hard timeout for exploration of a single function (seconds)"
-            <*> Crux.section
-              "explore-parallel"
-              Crux.yesOrNoSpec
-              False
-              "Explore different functions in parallel"
-            <*> Crux.section
-              "entry-points"
-              (Crux.listSpec Crux.stringSpec)
-              []
-              "Comma-separated list of functions to examine."
-            <*> Crux.section
-              "skip-functions"
-              (Crux.listSpec Crux.stringSpec)
-              []
-              "List of functions to skip during exploration"
-            <*> Crux.section
-              "verbosity"
-              Crux.numSpec
-              0
-              "Verbosity of logging. (0: minimal, 1: informational, 2: debug)",
+            <*> Crux.section "explore" Crux.yesOrNoSpec False exploreDoc
+            <*> Crux.section "re-explore" Crux.yesOrNoSpec False reExploreDoc
+            <*> Crux.section "explore-budget" Crux.numSpec 8 exploreBudgetDoc
+            <*> Crux.section "explore-timeout" Crux.numSpec 5 exploreTimeoutDoc
+            <*> Crux.section "explore-parallel" Crux.yesOrNoSpec False exploreParallelDoc
+            <*> Crux.section "entry-points" (Crux.listSpec Crux.stringSpec) [] entryPointsDoc
+            <*> Crux.section "skip-functions" (Crux.listSpec Crux.stringSpec) [] skipDoc
+            <*> Crux.section "verbosity" Crux.numSpec 0 verbDoc,
         Crux.cfgEnv =
           map
             ( \envDescr ->
@@ -154,41 +148,20 @@ ucCruxLLVMConfig = do
             (Crux.cfgCmdLineFlag llvmOpts)
             ++ [ Crux.Option
                    []
-                   ["entry-points"]
-                   "List of functions to examine."
-                   $ Crux.ReqArg "FUN" $
-                     \v opts -> Right opts {entryPoints = v : entryPoints opts},
-                 Crux.Option
-                   ['v']
-                   ["verbosity"]
-                   "Verbosity of logging. (0: minimal, 1: informational, 2: debug)"
-                   $ Crux.ReqArg "LEVEL" $
-                     Crux.parsePosNum
-                       "LEVEL"
-                       $ \v opts ->
-                         opts {verbosity = v},
-                 Crux.Option
-                   []
-                   ["skip-functions"]
-                   "List of functions to skip during exploration"
-                   $ Crux.ReqArg "FUN" $
-                     \v opts -> Right opts {skipFunctions = v : skipFunctions opts},
-                 Crux.Option
-                   []
                    ["explore"]
-                   "Run in exploration mode"
+                   (Text.unpack exploreDoc)
                    $ Crux.NoArg $
                      \opts -> Right opts {doExplore = True},
                  Crux.Option
                    []
                    ["re-explore"]
-                   "Re-explore functions that have already been explored (i.e., have logs)"
+                   (Text.unpack reExploreDoc)
                    $ Crux.NoArg $
                      \opts -> Right opts {reExplore = True},
                  Crux.Option
                    []
                    ["explore-budget"]
-                   "Budget for exploration mode"
+                   (Text.unpack exploreBudgetDoc)
                    $ Crux.ReqArg
                      "INT"
                      $ Crux.parsePosNum
@@ -197,7 +170,7 @@ ucCruxLLVMConfig = do
                  Crux.Option
                    []
                    ["explore-timeout"]
-                   "Hard timeout for exploration of a single function (seconds)"
+                   (Text.unpack exploreTimeoutDoc)
                    $ Crux.ReqArg
                      "INT"
                      $ Crux.parsePosNum
@@ -206,8 +179,29 @@ ucCruxLLVMConfig = do
                  Crux.Option
                    []
                    ["explore-parallel"]
-                   "Explore different functions in parallel"
+                   (Text.unpack exploreParallelDoc)
                    $ Crux.NoArg $
-                     \opts -> Right opts {exploreParallel = True}
+                     \opts -> Right opts {exploreParallel = True},
+                 Crux.Option
+                   []
+                   ["entry-points"]
+                   (Text.unpack entryPointsDoc)
+                   $ Crux.ReqArg "FUN" $
+                     \v opts -> Right opts {entryPoints = v : entryPoints opts},
+                 Crux.Option
+                   []
+                   ["skip-functions"]
+                   (Text.unpack skipDoc)
+                   $ Crux.ReqArg "FUN" $
+                     \v opts -> Right opts {skipFunctions = v : skipFunctions opts},
+                 Crux.Option
+                   ['v']
+                   ["verbosity"]
+                   (Text.unpack verbDoc)
+                   $ Crux.ReqArg "LEVEL" $
+                     Crux.parsePosNum
+                       "LEVEL"
+                       $ \v opts ->
+                         opts {verbosity = v}
                ]
       }
