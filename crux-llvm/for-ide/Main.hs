190256209963fa7acb940e55dd6d2b4baa0ae504
Valentin Robert <val@galois.com>
2021-07-12 15:41:29 -0700

    crux-llvm-for-ide: add command-line flags

--- crux-llvm/for-ide/Main.hs
+++ crux-llvm/for-ide/Main.hs
@@ -4,7 +4,7 @@
 
 module Main (main) where
 
-import Control.Lens (makeLenses, view)
+import Control.Lens (makeLenses, set, view)
 import Crux (OutputConfig)
 import qualified Crux
 import Crux.Config.Common (CruxOptions)
@@ -15,11 +15,13 @@ import CruxLLVMMain
     withCruxLLVMLogging,
   )
 import qualified Data.Aeson as JSON
+import Data.Text as Text (Text, unpack)
 import qualified Lumberjack as LJ
 import qualified Network.WebSockets as WS
 import Paths_crux_llvm (version)
 import RealMain (makeMain)
 import System.Exit (ExitCode)
+import Text.Read (readEither)
 
 data ForIDEOptions = ForIDEOptions
   { _cruxLLVMOptions :: LLVMOptions,
@@ -28,6 +30,12 @@ data ForIDEOptions = ForIDEOptions
   }
 
 makeLenses ''ForIDEOptions
+
+ideHostDoc :: Text
+ideHostDoc = "Host where the IDE is listening"
+
+idePortDoc :: Text
+idePortDoc = "Port at which the IDE is listening"
 
 forIDEConfig :: IO (Crux.Config ForIDEOptions)
 forIDEConfig = do
@@ -41,14 +49,28 @@ forIDEConfig = do
               "ide-host"
               Crux.stringSpec
               "127.0.0.1"
-              "Host where the IDE is listening"
+              ideHostDoc
             <*> Crux.section
               "ide-port"
               Crux.numSpec
               0
-              "Port at which the IDE is listening",
+              idePortDoc,
         Crux.cfgEnv = Crux.liftEnvDescr cruxLLVMOptions <$> Crux.cfgEnv llvmOpts,
-        Crux.cfgCmdLineFlag = Crux.liftOptDescr cruxLLVMOptions <$> Crux.cfgCmdLineFlag llvmOpts
+        Crux.cfgCmdLineFlag =
+          (Crux.liftOptDescr cruxLLVMOptions <$> Crux.cfgCmdLineFlag llvmOpts)
+            ++ [ Crux.Option
+                   []
+                   ["ide-host"]
+                   (Text.unpack ideHostDoc)
+                   $ Crux.ReqArg "STR" $
+                     \v opts -> Right (set ideHost v opts),
+                 Crux.Option
+                   []
+                   ["ide-port"]
+                   (Text.unpack idePortDoc)
+                   $ Crux.ReqArg "INT" $
+                     \v opts -> set idePort <$> readEither v <*> pure opts
+               ]
       }
 
 mainWithOutputConfig ::
