2a61ab8dc759379ef691fedf5173dacdb3923c02
Langston Barrett <langston.barrett@gmail.com>
2021-09-02 11:51:00 -0700

    uc-crux-llvm: Add function 'tryMakeEntryPoints'
    This should be a nicer entrypoint for creating 'EntryPoints' when using
    uc-crux-llvm as a library because it defers error handling to callers.

--- uc-crux-llvm/src/UCCrux/LLVM/Run/EntryPoints.hs
+++ uc-crux-llvm/src/UCCrux/LLVM/Run/EntryPoints.hs
@@ -11,10 +11,14 @@ module UCCrux.LLVM.Run.EntryPoints
   ( EntryPoints,
     makeEntryPoints,
     getEntryPoints,
+    tryMakeEntryPoints,
     makeEntryPointsOrThrow
   )
 where
 
+import           Data.Either (partitionEithers)
+import           Data.List.NonEmpty (NonEmpty, nonEmpty)
+import qualified Data.List.NonEmpty as NonEmpty
 import qualified Text.LLVM.AST as L
 
 import           Crux.LLVM.Config (throwCError, CError(MissingFun))
@@ -34,13 +38,28 @@ makeEntryPoints = EntryPoints
 getEntryPoints :: EntryPoints m -> [DeclSymbol m]
 getEntryPoints = doGetEntryPoints
 
+tryMakeEntryPoints ::
+  DeclMap m a ->
+  [FunctionName] ->
+  Either (NonEmpty FunctionName) (EntryPoints m)
+tryMakeEntryPoints declMap funs =
+  let (failures, successes) =
+        partitionEithers
+          (map
+            (\nm ->
+              case makeDeclSymbol (L.Symbol (functionNameToString nm)) declMap of
+                Nothing -> Left nm
+                Just d -> Right d)
+            funs)
+  in case nonEmpty failures of
+       Just nonEmptyFailures -> Left nonEmptyFailures
+       Nothing -> Right (makeEntryPoints successes)
+
 -- | Construct a 'EntryPoints' out of a user-supplied list of function names. If
 -- a function can't be found, throw a user error.
 makeEntryPointsOrThrow :: DeclMap m a -> [FunctionName] -> IO (EntryPoints m)
-makeEntryPointsOrThrow declMap =
-  fmap makeEntryPoints .
-    mapM
-      (\nm ->
-        case makeDeclSymbol (L.Symbol (functionNameToString nm)) declMap of
-          Nothing -> throwCError (MissingFun (functionNameToString nm))
-          Just d -> pure d)
+makeEntryPointsOrThrow declMap funs =
+  case tryMakeEntryPoints declMap funs of
+    Left errs ->
+      throwCError (MissingFun (functionNameToString (NonEmpty.head errs)))
+    Right entries -> return entries
