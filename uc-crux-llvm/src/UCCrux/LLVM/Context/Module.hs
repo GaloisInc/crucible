0191919939422371f12a47140bcc74f885c0a2a0
Langston Barrett <langston.barrett@gmail.com>
2021-09-13 12:25:25 -0700

    uc-crux-llvm: Add a map of function declarations to the ModuleContext

--- uc-crux-llvm/src/UCCrux/LLVM/Context/Module.hs
+++ uc-crux-llvm/src/UCCrux/LLVM/Context/Module.hs
@@ -63,7 +63,7 @@ import           UCCrux.LLVM.FullType.Translation (TranslatedTypes(..), TypeTran
 import           UCCrux.LLVM.FullType.Type (FullTypeRepr, ModuleTypes, MapToCrucibleType)
 import           UCCrux.LLVM.FullType.ReturnType (ReturnType(..), ReturnTypeToCrucibleType)
 import           UCCrux.LLVM.FullType.VarArgs (VarArgsRepr, varArgsReprToBool)
-import           UCCrux.LLVM.Module (Module, FuncSymbol, DeclMap, DefnMap, FuncMap, GlobalMap, funcSymbol, getFuncSymbol, funcMapDecls, funcMapDefns)
+import           UCCrux.LLVM.Module (Module, FuncSymbol, DeclMap, DefnMap, FuncMap, GlobalMap, funcSymbol, getFuncSymbol, funcMapDecls, funcMapDefns, allModuleDeclMap)
 {- ORMOLU_ENABLE -}
 
 -- | The @m@ type parameter represents a specific LLVM module
@@ -73,7 +73,8 @@ data ModuleContext m arch = ModuleContext
     _moduleTypes :: ModuleTypes m,
     _globalTypes :: GlobalMap m (Some (FullTypeRepr m)),
     _funcTypes :: FuncMap m (FunctionTypes m arch),
-    _moduleTranslation :: ModuleTranslation arch
+    _moduleTranslation :: ModuleTranslation arch,
+    _moduleDecls :: FuncMap m L.Declare
   }
 
 moduleFilePath :: Simple Lens (ModuleContext m arch) FilePath
@@ -100,6 +101,9 @@ defnTypes = funcTypes . funcMapDefns
 moduleTranslation :: Simple Lens (ModuleContext m arch) (ModuleTranslation arch)
 moduleTranslation = lens _moduleTranslation (\s v -> s {_moduleTranslation = v})
 
+moduleDecls :: Simple Lens (ModuleContext m arch) (FuncMap m L.Declare)
+moduleDecls = lens _moduleDecls (\s v -> s {_moduleDecls = v})
+
 dataLayout :: Getter (ModuleContext m arch) DataLayout
 dataLayout = moduleTranslation . LLVMTrans.transContext . LLVMTrans.llvmTypeCtx . to llvmDataLayout
 
@@ -121,7 +125,7 @@ makeModuleContext path llvmMod trans =
         Right (TranslatedTypes llvmMod' modTypes globTypes decTypes) ->
           Right $
             SomeModuleContext $
-              ModuleContext path llvmMod' modTypes globTypes decTypes trans
+              ModuleContext path llvmMod' modTypes globTypes decTypes trans (allModuleDeclMap llvmMod')
 
 -- ------------------------------------------------------------------------------
 -- Looking up CFGs
