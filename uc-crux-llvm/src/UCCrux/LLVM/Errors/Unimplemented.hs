d4d8d84605931dd074db31889ea929de67f92b73
Langston Barrett <langston.barrett@gmail.com>
2021-10-08 08:24:57 -0400

    uc-crux-llvm: Remove a few unused constructors of 'Unimplemented' (#879)

--- uc-crux-llvm/src/UCCrux/LLVM/Errors/Unimplemented.hs
+++ uc-crux-llvm/src/UCCrux/LLVM/Errors/Unimplemented.hs
@@ -22,8 +22,6 @@ where
 
 {- ORMOLU_DISABLE -}
 import Control.Exception (SomeException, try, displayException)
-import Data.Text (Text)
-import qualified Data.Text as Text
 import Panic hiding (panic)
 import qualified Panic
 {- ORMOLU_ENABLE -}
@@ -34,13 +32,9 @@ data Unimplemented
   | OpaqueType
   | UnsupportedType
   | MetadataType
-  | GeneratingArrays
-  | IndexCursor
-  | ConstrainGlobal
   | GetHostNameNegativeSize
   | GetHostNameSmallSize
   | NonEmptyUnboundedSizeArrays
-  | NonVoidUndefinedFunc Text
   | CastIntegerToPointer
   deriving (Eq, Ord)
 
@@ -52,14 +46,9 @@ ppUnimplemented =
     OpaqueType -> "Opaque (undefined) types in globals or arguments"
     UnsupportedType -> "Unsupported types in globals or arguments"
     MetadataType -> "LLVM metadata types in globals or arguments"
-    GeneratingArrays -> "Arrays in globals or arguments"
-    IndexCursor -> "Deduced preconditions on array elements"
-    ConstrainGlobal -> "Constraints on a global variable"
     GetHostNameNegativeSize -> "`gethostname` called with a negative length"
     GetHostNameSmallSize -> "`gethostname` called with a small length"
     NonEmptyUnboundedSizeArrays -> "Generating arrays with unbounded size"
-    NonVoidUndefinedFunc func ->
-      "Non-void function without a definition: " ++ Text.unpack func
     CastIntegerToPointer -> "Value of integer type treated as/cast to a pointer"
 
 instance PanicComponent Unimplemented where
