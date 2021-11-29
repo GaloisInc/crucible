90df647f9cefd753bc1f053ee1de5e8814dc48a5
Langston Barrett <langston.barrett@gmail.com>
2021-11-29 11:18:41 -0500

    uc-crux-llvm: Add a callstack to Bug
    This isn't really used yet, but in the future will hopefully be useful to users
    in comprehending how a given error occurred during simulation.

--- uc-crux-llvm/src/UCCrux/LLVM/Run/Simulate.hs
+++ uc-crux-llvm/src/UCCrux/LLVM/Run/Simulate.hs
@@ -537,6 +537,7 @@ mkCallbacks appCtx modCtx funCtx halloc callbacks constraints cfg llvmOpts =
                 (Map.union argAnnotations retAnns)
                 argShapes
                 badBehavior
+                callStack
                 >>= IORef.modifyIORef explRef . (:)
         return mempty
 
