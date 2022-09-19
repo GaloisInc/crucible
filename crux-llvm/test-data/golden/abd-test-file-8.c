ff037654ad5a9d4dfcdb99fefed3f91fbab54431
arjunvish <arjun.viswanathan23@gmail.com>
2022-09-19 10:52:10 -0500

    Adding tests for abduction

---
+++ crux-llvm/test-data/golden/abd-test-file-8.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <crucible.h>
+
+int8_t f(int8_t x) {
+  return x + 1;
+}
+
+int main() {
+  int8_t x = crucible_int8_t("x");
+  assuming(x < 100);
+  check(f(x) < 100);
+  return 0;
+}
