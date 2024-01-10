50e83e97aebfbdfae817a4676311c4caa635bfc3
Langston Barrett <langston.barrett@gmail.com>
2024-01-10 12:59:00 -0500

    llvm: Haddocks for callstacks

--- crucible-llvm/src/Lang/Crucible/LLVM/MemModel/CallStack/Internal.hs
+++ crucible-llvm/src/Lang/Crucible/LLVM/MemModel/CallStack/Internal.hs
@@ -26,16 +26,20 @@ newtype FunctionName =
   FunctionName { getFunctionName :: Text }
   deriving (Eq, Monoid, Ord, Semigroup)
 
+-- | Call stacks (lists of function names), mostly for diagnostics
 newtype CallStack =
   CallStack { runCallStack :: Seq FunctionName }
   deriving (Eq, Monoid, Ord, Semigroup)
 
+-- | Add a function name to the top of the call stack
 cons :: FunctionName -> CallStack -> CallStack
 cons top (CallStack rest) = CallStack (top Seq.<| rest)
 
+-- | Is this 'CallStack' empty?
 null :: CallStack -> Bool
 null = Seq.null . runCallStack
 
+-- | Summarize the 'StackFrame's of a 'MemState' into a 'CallStack'
 getCallStack :: MemState sym -> CallStack
 getCallStack =
   \case
@@ -43,6 +47,7 @@ getCallStack =
     StackFrame _ _ nm _ rest -> cons (FunctionName nm) (getCallStack rest)
     BranchFrame _ _ _ rest -> getCallStack rest
 
+-- | Pretty-print a call stack (one function per line)
 ppCallStack :: CallStack -> PP.Doc ann
 ppCallStack =
   PP.vsep . toList . fmap (PP.pretty . getFunctionName) . runCallStack
