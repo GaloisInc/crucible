a9d50470565809d370056ab5bade77ba6a71410d
Langston Barrett <langston.barrett@gmail.com>
2025-03-07 12:19:18 -0500

    Add tests for the Crucible `Backend`

--- crucible/test/helpers/Main.hs
+++ crucible/test/helpers/Main.hs
@@ -6,14 +6,41 @@ import Data.List (isInfixOf)
 import Test.Hspec
 import Test.Tasty
 import Test.Tasty.Hspec (testSpec)
+import qualified Test.Tasty.HUnit as TTH
 
+import Data.Parameterized.Nonce (newIONonceGenerator)
+import Data.Parameterized.Some (Some(Some))
+import Lang.Crucible.Backend (SomeBackend(..))
+import qualified Lang.Crucible.Backend as LCB
+import Lang.Crucible.Backend.Simple (newSimpleBackend)
 import Lang.Crucible.Panic
+import What4.Expr (EmptyExprBuilderState(..))
+import What4.Expr.Builder (newExprBuilder)
+import qualified What4.Interface as W4I
+import What4.FloatMode (FloatModeRepr(FloatIEEERepr))
 
 import qualified Panic as P
 
 main :: IO ()
 main =
   defaultMain =<< panicTests
+
+mkBackend :: IO (Some SomeBackend)
+mkBackend = do
+  Some nonceGen <- newIONonceGenerator
+  sym <- newExprBuilder FloatIEEERepr EmptyExprBuilderState nonceGen
+  Some . SomeBackend <$> newSimpleBackend sym
+
+backendTests :: TestTree
+backendTests =
+  testGroup
+  "Backend"
+  [ TTH.testCase "push/pop" $ do
+      Some (SomeBackend bak) <- mkBackend
+      asmps <- LCB.popAssumptionFrame bak =<< LCB.pushAssumptionFrame bak
+      p <- LCB.assumptionsPred (LCB.backendGetSym bak) asmps
+      Just True TTH.@=? W4I.asConstantPred p
+  ]
 
 panicTests :: IO TestTree
 panicTests =
