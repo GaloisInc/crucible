af516ddaf16b03e699bb21525b7d56a168ab2e9a
Stuart Pernsteiner <spernsteiner@galois.com>
2025-04-16 13:07:14 -0700

    crucible-mir: fix override path for core::intrinsics::bswap

--- crucible-mir/src/Mir/TransCustom.hs
+++ crucible-mir/src/Mir/TransCustom.hs
@@ -993,7 +993,7 @@ mem_crucible_identity_transmute = (["core","mem", "crucible_identity_transmute"]
     )
 
 mem_bswap ::  (ExplodedDefId, CustomRHS)
-mem_bswap = (["core", "intrinsics", "", "bswap"],
+mem_bswap = (["core", "intrinsics", "bswap"],
     \ substs -> case substs of
       Substs [tyT] -> Just $ CustomOp $ \ _ ops -> case ops of
         [e@(MirExp argTy@C.BVRepr{} argExpr)] -> return . MirExp argTy $ bswap argTy argExpr
