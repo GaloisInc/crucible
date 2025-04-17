6c5492284f2374a6ba598fe2e9db66a333d539a0
Stuart Pernsteiner <spernsteiner@galois.com>
2025-04-17 13:13:03 -0700

    crux-mir: fix #[crux_test] -> #[crux::test] in transmute_bytes test case

--- crux-mir/test/conc_eval/mem/transmute_bytes.rs
+++ crux-mir/test/conc_eval/mem/transmute_bytes.rs
@@ -1,6 +1,6 @@
 use std::mem::transmute;
 
-#[cfg_attr(crux, crux_test)]
+#[cfg_attr(crux, crux::test)]
 fn crux_test() -> u8 {
     assert_eq!(unsafe { transmute::<u32, [u8; 4]>(0x01020304) }, [4, 3, 2, 1]);
     assert_eq!(unsafe { transmute::<[u8; 4], u32>([4, 3, 2, 1]) }, 0x01020304);
