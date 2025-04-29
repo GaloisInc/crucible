31740f5f728e1e9bdb57e3ff3518c9eba2350103
Stuart Pernsteiner <spernsteiner@galois.com>
2025-04-29 14:38:15 -0700

    crux-mir: add test case for "transConstVal (ConstFnPtr): Couldn't resolve call_once"

---
+++ crux-mir/test/conc_eval/clos/closure_to_fn_ptr_const.rs
@@ -0,0 +1,12 @@
+
+static F: fn() -> i32 = const { || 123 };
+
+#[cfg_attr(crux, crux::test)]
+fn crux_test() -> i32 {
+    let g: fn() -> i32 = const { || 123 };
+    F() + g()
+}
+
+pub fn main() {
+    println!("{:?}", crux_test());
+}
