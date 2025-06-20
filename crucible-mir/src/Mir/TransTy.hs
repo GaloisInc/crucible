6db0b8f588af97e5ee132006d5e133eda75c4d65
Sam Cowger <sam@galois.com>
2025-06-20 13:30:11 -0700

    crucible-mir: declare `StructInfo` with GADT syntax
    This matches the vast majority of existing GADTs in these packages, and should
    make for cleaner diffs when adding new variants.

--- crucible-mir/src/Mir/TransTy.hs
+++ crucible-mir/src/Mir/TransTy.hs
@@ -673,11 +673,14 @@ adjustFieldData (FkMaybe tpr) f e =
     adjustJust' tpr "adjustFieldData: expected Just, but got Nothing" f e
 
 
-data StructInfo = forall ctx tp tp'. StructInfo
-    (C.CtxRepr ctx)
-    (Ctx.Index ctx tp')
-    (FieldKind tp tp')
-    Bool -- ^ `True` iff the described field is the struct's last
+data StructInfo where
+  StructInfo ::
+    C.CtxRepr ctx ->
+    Ctx.Index ctx tp' ->
+    FieldKind tp tp' ->
+    -- | `True` iff the described field is the struct's last
+    Bool ->
+    StructInfo
 
 -- First argument is `True` if a wrapper is expected.
 checkFieldKind :: Bool -> C.TypeRepr tp -> C.TypeRepr tp' -> String ->
