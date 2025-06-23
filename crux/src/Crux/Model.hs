d5780f36cc74dac5620d876bc565a376f3008cf3
Michal Podhradsky <mpodhradsky@galois.com>
2025-06-23 16:32:31 -0700

    Refactor `showEnt''` such that it calls `showBVLiteral{Signed,Unsigned,Decimal}` directly

--- crux/src/Crux/Model.hs
+++ crux/src/Crux/Model.hs
@@ -36,6 +36,13 @@ toDouble = fromRational
 
 data BVRepr = HexSigned | HexUnsigned | Decimal
 
+showBVLiteral :: (1 <= w) => NatRepr w -> BV w -> String
+showBVLiteral w bv =
+    (if x < 0 then "-0x" else "0x") ++ N.showHex i (if natValue w == 64 then "L" else "")
+  where
+  x = BV.asSigned w bv
+  i = abs x
+
 showBVLiteralSigned :: (1 <= w) => NatRepr w -> BV w -> String
 showBVLiteralSigned w bv =
     (if x < 0 then "-0x" else "0x") ++ N.showHex i (if natValue w == 64 then "L" else "")
@@ -54,13 +61,6 @@ showBVLiteralDecimal w bv =
     show x
   where
   x = BV.asSigned w bv
-
-showBVLiteral :: (1 <= w) => NatRepr w -> BV w -> BVRepr -> String
-showBVLiteral w bv repr =
-  case repr of
-    HexSigned -> showBVLiteralSigned w bv
-    HexUnsigned -> showBVLiteralUnsigned w bv
-    Decimal -> showBVLiteralDecimal w bv
 
 showFloatLiteral :: BigFloat -> String
 showFloatLiteral x
@@ -81,7 +81,7 @@ showDoubleLiteral x
 valsJS :: BaseTypeRepr ty -> Vals ty -> IO [JS]
 valsJS ty (Vals xs) =
   let showEnt = case ty of
-        BaseBVRepr n -> showEnt'' (showBVLiteral n) n
+        BaseBVRepr n -> showEnt'' n
         BaseFloatRepr (FloatingPointPrecisionRepr eb sb)
           | Just Refl <- testEquality eb (knownNat @8)
           , Just Refl <- testEquality sb (knownNat @24)
@@ -105,17 +105,18 @@ valsJS ty (Vals xs) =
          , "val"  ~> jsStr (repr (entryValue e))
          , "bits" ~> jsStr (show n)
          ]
-  showEnt'' :: Show b => (a -> BVRepr -> String) -> b -> Entry a -> IO JS
-  showEnt'' repr n e =
-    do l <- fromMaybe jsNull <$> jsLoc (entryLoc e)
-       pure $ jsObj
-         [ "name" ~> jsStr (entryName e)
-         , "loc"  ~> l
-         , "val"  ~> jsStr (repr (entryValue e) HexSigned)
-         , "val-unsigned"  ~> jsStr (repr (entryValue e) HexUnsigned)
-         , "val-decimal"  ~> jsStr (repr (entryValue e) Decimal)
-         , "bits" ~> jsStr (show n)
-         ]
+
+  showEnt'' :: (1 <= w) => NatRepr w -> Entry (BV w) -> IO JS
+  showEnt'' n e = do
+    l <- fromMaybe jsNull <$> jsLoc (entryLoc e)
+    pure $ jsObj
+      [ "name"         ~> jsStr (entryName e)
+      , "loc"          ~> l
+      , "val"          ~> jsStr (showBVLiteralSigned n (entryValue e))
+      , "val-unsigned" ~> jsStr (showBVLiteralUnsigned n (entryValue e))
+      , "val-decimal"  ~> jsStr (showBVLiteralDecimal n (entryValue e))
+      , "bits"         ~> jsStr (show n)
+      ]
 
 modelJS :: ModelView -> IO JS
 modelJS m =
