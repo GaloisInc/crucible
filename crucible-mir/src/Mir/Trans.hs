f7c5d1deca05a32627de6ec014ff75f3de54c370
Sam Cowger <sam@galois.com>
2025-08-08 13:56:55 -0700

    crucible-mir: support subslice projections on slices
    Also, provide a few notes on a potential implementation of subslice projections
    on arrays.

--- crucible-mir/src/Mir/Trans.hs
+++ crucible-mir/src/Mir/Trans.hs
@@ -1618,6 +1618,46 @@ evalPlaceProj _ pl (M.Downcast _idx) = return pl
 -- Subtype is a no-op, as it is only present in the MIR to making subtyping
 -- explicit during optimizations and codegen.
 evalPlaceProj _ pl (M.Subtype _ty) = return pl
+-- Subslicing is defined on slices and arrays. See the haddock for `Subslice`
+-- for details on the semantics of `fromIndex` and `toIndex`.
+evalPlaceProj ty (MirPlace tpr ref meta) (M.Subslice fromIndex toIndex fromEnd) =
+  case (ty, ref, tpr, meta) of
+    (M.TySlice _elemTy, headRef, elemTpr, SliceMeta len) ->
+      let lastIndex = mkLastIndex len
+          newLen = R.App (lastIndex `usizeSub` firstIndex)
+          newMeta = SliceMeta newLen
+      in MirPlace elemTpr <$> mirRef_offset headRef firstIndex <*> pure newMeta
+
+    -- TODO: Crucible's representation of `MirReference`s to arrays is not
+    -- expressive enough to implement this case properly. A Crucible reference
+    -- to an array is a reference to the entire array, and cannot have
+    -- `mirRef_offset` applied to it - unlike slices, a Crucible reference to
+    -- which is represented as a reference to its first element.
+
+    -- A couple possible ways past this:
+    -- - Make Crucible array references actually point to the first element of
+    --   the array, mimicking Crucible's approach to slices and Rust itself's
+    --   approach to arrays. Then, they'd be amenable to `mirRef_offset`.
+    -- - Implement a new reference path extension, something like
+    --   "MirRef_Subarray". Such a path extension would behave somewhat like
+    --   `MirRef_Index`, but instead of offering read/write access to a single
+    --   element of the parent array, it would offer read/write access to a
+    --   subarray of elements of the parent array.
+    --
+    -- NB: after implementing this, update `Mir.Mir.typeOfProj`, which currently
+    -- declares that `Subslice` unconditionally yields a slice. This is
+    -- incorrect; `Subslice` applied to an array will yield an array.
+    (M.TyArray {}, _, _, _) -> mirFail
+      "evalPlaceProj: subslicing not yet supported on arrays"
+
+    _ -> mirFail $
+      "evalPlaceProj: subslicing not supported on " ++ show (ty, tpr, meta)
+  where
+    usize = R.App . usizeLit . fromIntegral
+    firstIndex = usize fromIndex
+    mkLastIndex len
+      | fromEnd = R.App (len `usizeSub` usize toIndex)
+      | otherwise = usize toIndex
 evalPlaceProj ty (MirPlace _ _ meta) proj =
     mirFail $ "projection " ++ show proj ++ " not yet implemented for " ++ show (ty, meta)
 
