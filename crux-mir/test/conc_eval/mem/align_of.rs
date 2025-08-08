8fb1a669a086bb04d59e732bf41a3bbf743fb0cb
Bretton <qsctr@users.noreply.github.com>
2025-08-07 20:16:54 -0700

    crux-mir: More size_of/align_of test cases

--- crux-mir/test/conc_eval/mem/align_of.rs
+++ crux-mir/test/conc_eval/mem/align_of.rs
@@ -1,8 +1,13 @@
+struct Thing {
+    x: u32,
+    y: i128,
+}
+
 #[cfg_attr(crux, crux::test)]
-fn crux_test() -> usize {
-    align_of::<u8>()
+fn crux_test() -> [usize; 4] {
+    [align_of::<u8>(), align_of::<(i16, i32)>(), align_of::<[u64; 3]>(), align_of::<Thing>()]
 }
 
 pub fn main() {
-    println!("{}", crux_test());
+    println!("{:?}", crux_test());
 }
--- crux-mir/test/conc_eval/mem/size_of.rs
+++ crux-mir/test/conc_eval/mem/size_of.rs
@@ -1,8 +1,13 @@
+struct Thing {
+    x: u32,
+    y: i128,
+}
+
 #[cfg_attr(crux, crux::test)]
-fn crux_test() -> usize {
-    size_of::<i32>()
+fn crux_test() -> [usize; 4] {
+    [size_of::<u8>(), size_of::<(i16, i32)>(), size_of::<[u64; 3]>(), size_of::<Thing>()]
 }
 
 pub fn main() {
-    println!("{}", crux_test());
+    println!("{:?}", crux_test());
 }
