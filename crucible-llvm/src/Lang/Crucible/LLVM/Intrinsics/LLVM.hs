GaloisInc/crucible

commit 6eca1935b6e7d1b56934ec3d03d2c9f5091f2760

Use new what4 bitvector helpers, hlint in crux-{llvm,mir}

crucible-llvm/src/Lang/Crucible/LLVM/Intrinsics/LLVM.hs
@@ -1716,16 +1716,16 @@ callIsFpclass ::
 callIsFpclass regOp@(regValue -> op) (regValue -> test) = do
   sym <- getSymInterface
   let w1 = knownNat @1
-  bvOne  <- liftIO $ bvLit sym w1 (BV.one w1)
-  bvZero <- liftIO $ bvLit sym w1 (BV.zero w1)
+  bv1 <- liftIO $ bvZero sym w1
+  bv0 <- liftIO $ bvOne sym w1
 
   let negative bit = liftIO $ do
         isNeg <- iFloatIsNeg @_ @fi sym op
-        liftIO $ bvIte sym isNeg bit bvZero
+        liftIO $ bvIte sym isNeg bit bv0
 
   let positive bit = liftIO $ do
         isPos <- iFloatIsPos @_ @fi sym op
-        liftIO $ bvIte sym isPos bit bvZero
+        liftIO $ bvIte sym isPos bit bv0
 
   let negAndPos doCheck = liftIO $ do
         check <- doCheck
@@ -1735,19 +1735,19 @@ callIsFpclass regOp@(regValue -> op) (regValue -> test) = do
 
   let callIsInf x = do
         isInf <- iFloatIsInf @_ @fi sym x
-        bvIte sym isInf bvOne bvZero
+        bvIte sym isInf bv1 bv0
 
   let callIsNormal x = do
         isNorm <- iFloatIsNorm @_ @fi sym x
-        bvIte sym isNorm bvOne bvZero
+        bvIte sym isNorm bv1 bv0
 
   let callIsSubnormal x = do
         isSubnorm <- iFloatIsSubnorm @_ @fi sym x
-        bvIte sym isSubnorm bvOne bvZero
+        bvIte sym isSubnorm bv1 bv0
 
   let callIsZero x = do
         is0 <- iFloatIsZero @_ @fi sym x
-        bvIte sym is0 bvOne bvZero
+        bvIte sym is0 bv1 bv0
 
   isNan <- Libc.callIsnan w1 regOp
   (isInfN, isInfP) <- negAndPos $ callIsInf op
@@ -1758,9 +1758,9 @@ callIsFpclass regOp@(regValue -> op) (regValue -> test) = do
   foldM
     (\bits (bitNum, check) -> liftIO $ do
         isBitSet <- liftIO $ testBitBV sym bitNum test
-        newBit <- liftIO $ bvIte sym isBitSet check bvZero
+        newBit <- liftIO $ bvIte sym isBitSet check bv0
         liftIO $ bvOrBits sym newBit bits)
-    bvZero
+    bv0
     [ (0, isNan)      -- Signaling NaN
     , (1, isNan)      -- Quiet NaN
     , (2, isInfN)     -- Negative infinity

.github/workflows/lint.yml
@@ -32,3 +32,5 @@ jobs:
           (cd crucible-symio/; ../hlint-3.8/hlint src tests)
           (cd crucible-syntax/; ../hlint-3.8/hlint src test)
           (cd crucible-wasm/; ../hlint-3.8/hlint src test)
+          (cd crux-llvm/; ../hlint-3.8/hlint src test)
+          (cd crux-mir/; ../hlint-3.8/hlint src test)

crux-llvm/src/Crux/LLVM/Overrides.hs
@@ -34,7 +34,7 @@ import Data.Parameterized.Context(pattern Empty, pattern (:>), singleton)
 import What4.ProgramLoc( Position(..), ProgramLoc(..) )
 import What4.Symbol(userSymbol, emptySymbol, safeSymbol)
 import What4.Interface
-          (freshConstant, bvLit, bvAdd, asBV, predToBV,
+          (freshConstant, bvLit, bvOne, bvAdd, asBV, predToBV,
           getCurrentProgramLoc, printSymExpr, arrayUpdate, bvIsNonzero)
 
 import Lang.Crucible.Types
@@ -417,7 +417,7 @@ fresh_str arch mvar (Empty :> pName :> maxLen) =
 
     -- Compute the allocation length, which is the requested length plus one
     -- to hold the NUL terminator
-    one <- liftIO $ bvLit sym ?ptrWidth (BV.one ?ptrWidth)
+    one <- liftIO $ bvOne sym ?ptrWidth
     -- maxLenBV <- liftIO $ projectLLVM_bv sym (regValue maxLen)
     len <- liftIO $ bvAdd sym (regValue maxLen) one
     mem0 <- readGlobal mvar

crux-llvm/src/Crux/LLVM/Simulate.hs
@@ -13,7 +13,6 @@
 
 module Crux.LLVM.Simulate where
 
-import qualified Data.BitVector.Sized as BV
 import Data.String (fromString)
 import qualified Data.Map.Strict as Map
 import Data.IORef
@@ -37,7 +36,7 @@ import Prettyprinter
 
 -- what4
 import qualified What4.Expr.Builder as WEB
-import What4.Interface (bvLit, natLit)
+import What4.Interface (bvLit, natLit, bvZero)
 
 -- crucible
 import Lang.Crucible.Backend
@@ -339,7 +338,7 @@ checkFun llvmOpts trans memVar =
       mem <- case lookupGlobal memVar gs of
                Just mem -> pure mem
                Nothing  -> fail "checkFun.checkMainWithArguments: Memory missing from global vars"
-      argc <- liftIO $ LLVMPointer <$> natLit sym 0 <*> bvLit sym w (BV.zero w)
+      argc <- liftIO $ LLVMPointer <$> natLit sym 0 <*> bvZero sym w
       sz   <- liftIO $ bvLit sym PtrWidth $ bytesToBV PtrWidth tp_sz
       (argv, mem') <- liftIO $ doAlloca bak mem sz alignment loc
       stateTree.actFrame.gpGlobals %= insertGlobal memVar mem'

crux-mir/src/Mir/Overrides.hs
@@ -229,7 +229,7 @@ regEval bak baseEval tpr v = go tpr v
 
         let vec = MirVector_Vector $ V.fromList vals
         let vecRef = newConstMirRef sym (MirVectorRepr tpr') vec
-        ptr <- subindexMirRefSim tpr' vecRef =<< liftIO (bvLit sym knownRepr (BV.zero knownRepr))
+        ptr <- subindexMirRefSim tpr' vecRef =<< liftIO (bvZero sym knownRepr)
         return $ Empty :> RV ptr :> RV len'
 
     go (FloatRepr fi) v = pure v

commit 39dd7acfb37f337764db0ffcc6913a6b9829dca4

crucible-llvm: Fix the semantics of the llvm.is.fpclass override
Fix a couple of subtle typos (introduced in
https://github.com/GaloisInc/crucible/commit/6eca1935b6e7d1b56934ec3d03d2c9f5091f2760)
that causes the definitions of 1-bit zero and one to be swapped, resulting in
the semantics for the `llvm.is.fpclass` intrinsic to be utterly wrong. This
allows the `llvm.is.fpclass` test case to succeed on Clang 17 or later:

```
$ PATH=~/Software/clang+llvm-17.0.2/bin:$PATH cabal -v0 run crux-llvm-test -- -p llvm.is.fpclass
<snip>
crux-llvm
  clang 17.0.2
    llvm.is.fpclass.ll
      solver=z3
        loop-merging=loop
          clang-range=recent-clang
            z3 4.8.14
              llvm.is.fpclass
                llvm.is.fpclass crux run:    OK (0.14s)
                llvm.is.fpclass crux output: OK
        loop-merging=loopmerge
          clang-range=recent-clang
            z3 4.8.14
              llvm.is.fpclass
                llvm.is.fpclass crux run:    OK (0.15s)
                llvm.is.fpclass crux output: OK

All 4 tests passed (0.29s)
```

Fixes #1586.

crucible-llvm/src/Lang/Crucible/LLVM/Intrinsics/LLVM.hs
@@ -1933,8 +1933,8 @@ callIsFpclass ::
 callIsFpclass regOp@(regValue -> op) (regValue -> test) = do
   sym <- getSymInterface
   let w1 = knownNat @1
-  bv1 <- liftIO $ bvZero sym w1
-  bv0 <- liftIO $ bvOne sym w1
+  bv1 <- liftIO $ bvOne sym w1
+  bv0 <- liftIO $ bvZero sym w1
 
   let negative bit = liftIO $ do
         isNeg <- iFloatIsNeg @_ @fi sym op