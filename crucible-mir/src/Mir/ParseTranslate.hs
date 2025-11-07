ea3ec9920cd2715829b4d7b4a0ccb03548c31296
Ryan Scott <rscott@galois.com>
2025-11-07 07:40:00 -0500

    crucible-mir: Improve error messages involving MIR JSON schema version mismatches
    Previously, `parseMIR` would parse the entirety of a MIR JSON file upfront, and
    if there was an error during parsing, it would report that error before
    attempting to check if the MIR JSON schema versions match. Most of the time,
    this leads to inscrutable JSON parse errors instead of more comprehensible
    schema version mismatch errors, which isn't an ideal user experience.
    
    This commit tweaks `parseMIR` such that if it fails to parse the MIR JSON file,
    it will check for mismatched schema versions _before_ it throws a JSON parse
    error.
    
    Fixes #1618.

--- crucible-mir/src/Mir/ParseTranslate.hs
+++ crucible-mir/src/Mir/ParseTranslate.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 -----------------------------------------------------------------------
 -- |
 -- Module           : Mir.ParseTranslate
@@ -18,6 +21,7 @@ import Control.Lens hiding((<.>))
 import Control.Monad (unless, when)
 
 import qualified Data.Aeson as J
+import qualified Data.Aeson.KeyMap as KeyMap
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Map as M
 import qualified Data.Set as Set
@@ -30,7 +34,7 @@ import Prettyprinter (Pretty(..))
 import qualified Lang.Crucible.FunctionHandle as C
 
 
-import Mir.Mir (Collection(..), namedTys, version, tiTy, tiNeedsDrop, tiLayout)
+import Mir.Mir (Collection(..), namedTys, tiTy, tiNeedsDrop, tiLayout)
 import Mir.JSON ()
 import Mir.GenericOps (uninternTys)
 import Mir.Pass(rewriteCollection)
@@ -46,30 +50,79 @@ import Debug.Trace
 supportedSchemaVersion :: Word64
 supportedSchemaVersion = 5
 
+-- | Parse a MIR JSON file to a 'Collection'. If parsing fails, attempt to give
+-- a more informative error message if the MIR JSON schema version is
+-- mismatched.
 parseMIR :: (HasCallStack, ?debug::Int) =>
             FilePath
          -> B.ByteString
          -> IO Collection
 parseMIR path f = do
-  let c = (J.eitherDecode f) :: Either String Collection
-  case c of
-      Left msg -> fail $ "JSON Decoding of " ++ path ++ " failed: " ++ msg
-      Right col -> do
-        unless (col ^. version == supportedSchemaVersion) $
-          fail $ unlines
-            [ path ++ " uses an unsupported mir-json schema version: "
-                   ++ show (col ^. version)
-            , "This crux-mir release only supports schema version "
-              ++ show supportedSchemaVersion ++ "."
-            , "(See https://github.com/GaloisInc/mir-json/blob/master/SCHEMA_CHANGELOG.md"
-            , "for more details on what the schema version means.)"
-            ]
-        when (?debug > 5) $ do
-          traceM "--------------------------------------------------------------"
-          traceM $ "Loaded module: " ++ path
-          traceM $ show (pretty col)
-          traceM "--------------------------------------------------------------"
-        return $ uninternMir col
+    col <-
+      case J.eitherDecode @Collection f of
+        Left msg -> fallback msg
+        Right col -> pure col
+    when (?debug > 5) $ do
+      traceM "--------------------------------------------------------------"
+      traceM $ "Loaded module: " ++ path
+      traceM $ show (pretty col)
+      traceM "--------------------------------------------------------------"
+    return $ uninternMir col
+  where
+    -- If parsing fails, try to interpret the file as a raw JSON J.Value and
+    -- see if the schema version number (if one exists) matches. If they are
+    -- mismatched, then report this, as this is a pretty significant clue that
+    -- the user set up mir-json incorrectly. Otherwise, report a generic JSON
+    -- parse error.
+    fallback ::
+      forall a.
+      -- | The error message received upon failing to parse the JSON file as a
+      -- 'Collection'. Raise this error if all other attempts to report a more
+      -- specific error message fail.
+      String ->
+      IO a
+    fallback jsonParseErrMsg = do
+      -- First parse a J.Value, without looking at what the contents of the
+      -- J.Value are. This is a bit inefficient, as we already attempted to
+      -- parse the JSON file's contents earlier, and this will attempt to parse
+      -- it yet again. We accept the inefficiency here because we know that we
+      -- are going to report an error message regardless.
+      mirJson <-
+        case J.eitherDecode @J.Value f of
+          Left msg -> fail $ path ++ " is not a valid JSON file: " ++ msg
+          Right mirJson -> pure mirJson
+      -- Next, check that the J.Value is an J.Object.
+      mirJsonObj <-
+        case mirJson of
+          J.Object obj -> pure obj
+          _ -> fail $ path ++ " does not contain a JSON object"
+      -- Confirm that one of the J.Object's keys is "version".
+      mirJsonVer <-
+        case KeyMap.lookup "version" mirJsonObj of
+          Just mirJsonVer -> pure mirJsonVer
+          Nothing -> fail $ path ++ " does not contain a \"version\" field"
+      -- Validate that the "version" key maps to a number.
+      actualSchemaVersion <-
+        case J.fromJSON @Word64 mirJsonVer of
+          J.Error msg ->
+            fail $ path ++ " has a \"version\" field, but it is not a number: " ++ msg
+          J.Success actualSchemaVersion -> pure actualSchemaVersion
+      -- As a sanity-check, ensure that the number which "version" maps to is a
+      -- supported JSON schema version. If not, error out early.
+      unless (actualSchemaVersion == supportedSchemaVersion) $
+        fail $ unlines
+          [ path ++ " uses an unsupported mir-json schema version: "
+                 ++ show actualSchemaVersion
+          , "This crux-mir release only supports schema version "
+            ++ show supportedSchemaVersion ++ "."
+          , "(See https://github.com/GaloisInc/mir-json/blob/master/SCHEMA_CHANGELOG.md"
+          , "for more details on what the schema version means.)"
+          ]
+      -- If we have reached this point, then we know that the JSON file failed
+      -- to parse as a Collection despite the schema versions matching, so
+      -- something has gone awry. Report a generic JSON parse error to the user
+      -- as a last resort.
+      fail $ "JSON decoding of " ++ path ++ " failed: " ++ jsonParseErrMsg
 
 uninternMir :: Collection -> Collection
 uninternMir col =
