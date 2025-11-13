bd62a51714c38a764f9295e039324f3fdf8ace83
Iavor S. Diatchki <diatchki@galois.com>
2025-11-13 09:59:11 -0800

    Haddock cleanup, typos, words

--- crucible/src/Lang/Crucible/README.hs
+++ crucible/src/Lang/Crucible/README.hs
@@ -37,20 +37,20 @@ Types are encoded using [singletons](https://github.com/Galoisinc/parameterized-
 
 {- $
 The inhabitants of each type are specified via the type function 'RegValue'.
-We also have 'RegValue'' which is just a newtype wrapper around `RegValue`,
-because in Haskell type families may not be partially applied but `newtypes` can.
+We also have 'RegValue'' which is just a @newtype@ wrapper around 'RegValue',
+because in Haskell type families may not be partially applied but @newtype@s can.
 
 An important subset of the Crucible types are the base types (see 'BaseToType'),
 which is a backend specific type for the symbolic expression we can construct
 (see 'SymExpr' in [what4](https://github.com/Galoisinc/what4)).
-Only these types may contain variables. In practice, we always `what4`'s
+Only these types may contain variables. In practice, we always @what4@'s
 'Expr' type to represent symbolic expressions.
 
 Also, in some cases we use 'RegEntry' which
-is just a pair of a 'RegValue' and it's associated 'TypeRepr'.
+is just a pair of a 'RegValue' and its associated 'TypeRepr'.
 
 There's also 'BaseTerm', which is similar to 'RegEntry' but
-for base types---it contains a `what4`'s 'BaseTypeRepr' and a value of the corresponding
+for base types---it contains a @what4@'s 'BaseTypeRepr' and a value of the corresponding
 base type (usually;  the type is parameterized on exactly what we package
 with the type).
 -}
@@ -79,12 +79,13 @@ The state of a running simulator is described in "Lang.Crucible.Simulator.Execut
   * 'ExecState' is the current state of execution.
      We start with 'InitialState', and keep performing steps until we get
      to a 'ResultState'.
-  * As the simulator executes, it keeps track of its state in 'SimState'.
+  * As the simulator executes, it keeps track of its state in 'SimState',
+    which is stored in the current `ExecState`.
   * 'SimContext' is the part of the state that persists across branches
-    (e.g, after we explore the @then@ part of an @if2 statement, we'll
+    (e.g, after we explore the @then@ part of an @if@ statement, we'll
     roll back some of the state changes before simulating the @else@ part,
-    but the stuff in `SimContexts` persists).  An important part of the
-    'SimContext' is the simulator's backend (`_ctxBackend`), which is how the
+    but the data in 'SimContext's persists).  An important part of the
+    'SimContext' is the simulator's backend ('_ctxBackend'), which is how the
     simulator communicates with a solver, and builds symbolic expressions
     ('IsSymBackend').
 
