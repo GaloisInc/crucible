fb370704e83c8816146c2a955d6e9da25f140eee
ed sandberg <ed.sandberg@galois.com>
2025-12-15 09:56:10 -0600

    added an other export and added some notes on what the exports are for

--- crux-mir/src/Mir/Defaults.hs
+++ crux-mir/src/Mir/Defaults.hs
@@ -1,7 +1,15 @@
-module Mir.Defaults (defaultRustEditionFlag) where
+module Mir.Defaults
+  ( defaultRustEdition
+  , defaultRustEditionFlag
+  ) where
 
+-- | The default Rust language edition used by crux-mir when invoking Rust
+-- tooling (e.g., mir-json or rustc) on a standalone @.rs@ file.
+--
+-- Cargo-managed builds continue to use the edition specified in @Cargo.toml@.
 defaultRustEdition :: String
 defaultRustEdition = "2021"
 
+-- | The @--edition@ flag corresponding to 'defaultRustEdition'.
 defaultRustEditionFlag :: String
 defaultRustEditionFlag = "--edition=" ++ defaultRustEdition
