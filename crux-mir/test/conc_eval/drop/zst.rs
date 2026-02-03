98361be5504088195168458cbda8b9764186a43c
Stuart Pernsteiner <spernsteiner@galois.com>
2026-02-02 16:43:47 -0800

    crux-mir: add tests for load/store and drop of zsts

---
+++ crux-mir/test/conc_eval/drop/zst.rs
@@ -0,0 +1,27 @@
+//! Test drop for zero-sized types.
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static DROPPED: AtomicUsize = AtomicUsize::new(0);
+
+struct S;
+impl Drop for S {
+    fn drop(&mut self) {
+        DROPPED.fetch_add(1, Ordering::Relaxed);
+    }
+}
+
+fn f<T>(x: T) {}
+
+#[cfg_attr(crux, crux::test)]
+fn crux_test() -> usize {
+    {
+        let s = S;
+        let s2 = (S,);
+        f(s2);
+    }
+    DROPPED.load(Ordering::Relaxed)
+}
+
+pub fn main() {
+    println!("{:?}", crux_test());
+}
