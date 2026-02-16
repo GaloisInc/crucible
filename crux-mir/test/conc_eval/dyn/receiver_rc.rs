9751a638000e09b3ac7cbc3b65c3e3b365fc5018
Stuart Pernsteiner <spernsteiner@galois.com>
2026-02-16 13:23:29 -0800

    crux-mir: add tests for Box/Pin as dyn Trait receivers

---
+++ crux-mir/test/conc_eval/dyn/receiver_rc.rs
@@ -0,0 +1,23 @@
+// FAIL: requires custom DST support in mkVirtCall / dispatchFromDyn
+use std::rc::Rc;
+
+trait TRc {
+    fn m_rc(self: Rc<Self>) -> u32;
+}
+
+impl TRc for u32 {
+    fn m_rc(self: Rc<Self>) -> u32 {
+        *self
+    }
+}
+
+#[cfg_attr(crux, crux::test)]
+fn crux_test() -> u32 {
+    let d_rc1: Rc<u32> = Rc::new(27);
+    let d_rc2: Rc<dyn TRc> = d_rc1;
+    d_rc2.m_rc()
+}
+
+fn main() {
+    println!("{:?}", crux_test());
+}
