672c36a40f78f8b9456865babdbc8c810aac3a0d
Eric Mertens <emertens@galois.com>
2026-03-05 09:33:07 -0800

    Add assertions to the bool test file for ordering

--- crux-mir/test/conc_eval/prim/bool.rs
+++ crux-mir/test/conc_eval/prim/bool.rs
@@ -3,10 +3,33 @@ fn f(x: bool) -> bool {
     x ^ true
 }
 
+fn ord_ops() {
+    assert!(false < true);
+    assert!(!(true < true));
+    assert!(!(false < false));
+    assert!(!(true < false));
+    
+    assert!(false <= true);
+    assert!(true <= true);
+    assert!(false <= false);
+    assert!(!(true <= false));
+
+    assert!(!(false > true));
+    assert!(!(true > true));
+    assert!(!(false > false));
+    assert!(true > false);
+    
+    assert!(!(false >= true));
+    assert!(true >= true);
+    assert!(false >= false);
+    assert!(true >= false);
+}
+
 const ARG: bool = true;
 
 #[cfg(with_main)]
 pub fn main() {
-    println!("{:?}", f(ARG))
+    println!("{:?}", f(ARG));
+    ord_ops();
 }
 #[cfg(not(with_main))] #[cfg_attr(crux, crux::test)] fn crux_test() -> bool { f(ARG) }
