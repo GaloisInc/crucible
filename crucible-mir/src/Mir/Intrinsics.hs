c9740a401bc48955074d4710f094d9e9e317713a
Sam Cowger <sam@galois.com>
2026-03-18 09:58:01 -0700

    crucible-mir: a module for enum-related intrinsics
    Code movement only, NFCI

--- crucible-mir/src/Mir/Intrinsics.hs
+++ crucible-mir/src/Mir/Intrinsics.hs
@@ -31,6 +31,7 @@
 module Mir.Intrinsics
   ( module Mir.Intrinsics,
     module Mir.Intrinsics.Aggregate,
+    module Mir.Intrinsics.Enum,
     module Mir.Intrinsics.Size,
     module Mir.Intrinsics.Syntax,
   )
@@ -77,36 +78,9 @@ import           What4.Partial
 
 import           Mir.FancyMuxTree
 import           Mir.Intrinsics.Aggregate
+import           Mir.Intrinsics.Enum
 import           Mir.Intrinsics.Size
 import           Mir.Intrinsics.Syntax (MIR)
-
-
-
--- Rust enum representation
-
--- A Rust enum, whose variants have the types listed in `ctx`.
-type RustEnumType discrTp variantsCtx = StructType (RustEnumFields discrTp variantsCtx)
-type RustEnumFields discrTp variantsCtx = EmptyCtx ::> discrTp ::> VariantType variantsCtx
-
-data SomeRustEnumRepr where
-  SomeRustEnumRepr ::
-    !(TypeRepr discrTp) ->
-    !(CtxRepr variantsCtx) ->
-    SomeRustEnumRepr
-
-pattern RustEnumFieldsRepr :: () => ctx ~ RustEnumFields discrTp variantsCtx => TypeRepr discrTp -> CtxRepr variantsCtx -> CtxRepr ctx
-pattern RustEnumFieldsRepr discrTp variantsCtx = Empty :> discrTp :> VariantRepr variantsCtx
-pattern RustEnumRepr :: () => tp ~ RustEnumType discrTp variantsCtx => TypeRepr discrTp -> CtxRepr variantsCtx -> TypeRepr tp
-pattern RustEnumRepr discrTp variantsCtx = StructRepr (RustEnumFieldsRepr discrTp variantsCtx)
-
-mkRustEnum :: TypeRepr discrTp -> CtxRepr variantsCtx -> f discrTp -> f (VariantType variantsCtx) -> App ext f (RustEnumType discrTp variantsCtx)
-mkRustEnum discrTp variantsCtx discr variant = MkStruct (RustEnumFieldsRepr discrTp variantsCtx) (Empty :> discr :> variant)
-
-rustEnumDiscriminant :: TypeRepr discrTp -> f (RustEnumType discrTp variantsCtx) -> App ext f discrTp
-rustEnumDiscriminant discrTp e = GetStruct e i1of2 discrTp
-
-rustEnumVariant :: CtxRepr variantsCtx -> f (RustEnumType discrTp variantsCtx) -> App ext f (VariantType variantsCtx)
-rustEnumVariant variantsCtx e = GetStruct e i2of2 (VariantRepr variantsCtx)
 
 
 vectorGetUsize :: (TypeRepr tp) ->
