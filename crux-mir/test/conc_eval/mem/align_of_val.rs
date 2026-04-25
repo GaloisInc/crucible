a46fd318c1c6449d56df040a840d46f944bbe191
Stuart Pernsteiner <spernsteiner@galois.com>
2026-04-24 17:19:30 -0700

    crux-mir: implement size_of_val and align_of_val using vtable entries

--- crux-mir/test/conc_eval/mem/align_of_val.rs
+++ crux-mir/test/conc_eval/mem/align_of_val.rs
@@ -1,4 +1,4 @@
-// use std::fmt::Debug;
+use std::fmt::Debug;
 
 // pub struct C<T: ?Sized> {
 //     pub x: u32,
@@ -19,10 +19,11 @@ fn crux_test() {
     assert_eq!(1, align_of_val::<str>(&"r"));
     assert_eq!(1, align_of_val::<str>(&"ro"));
 
-    // TODO(#1614): The following are not currently supported:
-
     // Trait objects
-    // assert_eq!(1, align_of_val::<dyn Debug>(&()));
+    assert_eq!(1, align_of_val::<dyn Debug>(&()));
+    assert_eq!(4, align_of_val::<dyn Debug>(&123_i32));
+
+    // TODO(#1614): The following are not currently supported:
 
     // Custom DSTs with a slice as the last field
     // let sized: C<[u8; 8]> = C {
